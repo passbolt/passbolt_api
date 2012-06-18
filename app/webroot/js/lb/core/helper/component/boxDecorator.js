62a0a3622e49219db70acdcc6fc92086eb53096a
cedric <cedric@cedric-laptop.(none)>
2012-06-18 16:51:20 +0530

    Decorate the world. Our own implementation of the pattern decorator ! Remy be nice ;) It is a first shot !

---
+++ app/webroot/js/lb/core/helper/component/boxDecorator.js
@@ -0,0 +1,15 @@
+steal( 
+    'jquery/class'
+)
+.then( 
+    function($){
+//        alert('oh yeah');
+//        lb.core.helper.BoxDecorator = {
+//            'render': function(){
+//                //this._super();
+//                //create a function such a _super, something like _decorated
+//                this._decorator[0].f['render'].apply(this);
+//            }
+//        };
+    }
+);
