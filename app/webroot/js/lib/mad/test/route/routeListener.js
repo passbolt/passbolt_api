3a8a74052e99c6057dd1ec3445b1c4c5fa050e76
cedric <cedric@cedric-laptop>
2012-07-09 01:02:07 +0530

    PASSBOLT-131 - Bug in unit tests under selenium Selenium disables some functions such as window.location. Simulate the change with a classic link (unijambiste ninja style)

--- app/webroot/js/lib/mad/test/route/routeListener.js
+++ app/webroot/js/lib/mad/test/route/routeListener.js
@@ -1,6 +1,6 @@
 steal('funcunit', function(){
 	var testEnv = null;
-	var eventBus = null;
+	
 	module("event", {
 		// runs before each test
 		setup: function(){
@@ -10,8 +10,7 @@ steal('funcunit', function(){
 				testEnv.mad.controller.AppController.destroy();
 				testEnv.mad.controller.AppController.setNs(APP_NS_ID); // Create the div element which will embedd the event bus controller
 				testEnv.$('body').append('<div id="mad_test_EventBus" />');
-				eventBus = new mad.event.EventBus(testEnv.$('#mad_test_EventBus'));
-				testEnv.mad.eventBus = eventBus;
+				testEnv.mad.eventBus = new testEnv.mad.event.EventBus(testEnv.$('#mad_test_EventBus'));
 			});
 		},
 		// runs after each test
@@ -23,11 +22,13 @@ steal('funcunit', function(){
 	test('Route.RouteListener : check the application is well listening the hash changes (TODO CHECK WHY IT GETS 2 EVENTS ... and not in the application)', function(){
 		stop();
 		testEnv.mad.route.RouteListener.singleton();
+		testEnv.$('body').append('<div id="mad_test_EventBus" />');
+		testEnv.$('body').append('<a id="change_hash" href="#!extension/controller/action/p1/p2/p3">change hash : special ninja technique to work with selenium</a>');
 		testEnv.mad.eventBus.bind(testEnv.mad.APP_NS_ID+'_route_change', function(){
 			ok(true, 'The route listener well detect that the route changed '+testEnv.location.hash);
 			start();
 		});
-		testEnv.location.hash = '#!extension/controller/action/p1/p2/p3';
+		S('#change_hash').click();
 	});
 
 });
