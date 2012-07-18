01ad3d616291f291676749e47eee4ea750e21dc8
Stripthis <remy@anon>
2012-07-18 21:43:16 +0200

    generic validation rules

--- app/Model/AppModel.php
+++ app/Model/AppModel.php
@@ -5,30 +5,40 @@
  * This file is application-wide model file. You can put all
  * application-wide model-related methods here.
  *
- * PHP 5
- *
- * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
- * Copyright 2005-2012, Cake Software Foundation, Inc. (http://cakefoundation.org)
- *
- * Licensed under The MIT License
- * Redistributions of files must retain the above copyright notice.
- *
- * @copyright     Copyright 2005-2012, Cake Software Foundation, Inc. (http://cakefoundation.org)
- * @link          http://cakephp.org CakePHP(tm) Project
- * @package       app.Model
- * @since         CakePHP(tm) v 0.2.9
- * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
+ * @copyright     Copyright 2012, Passbolt.com
+ * @package       app.Model.AppModel
+ * @since         version 2.12.7
+ * @license       http://www.passbolt.com/license
  */
-
 App::uses('Model', 'Model');
-
-/**
- * Application model for Cake.
- *
- * Add your application-wide methods in the class below, your models
- * will inherit them.
- *
- * @package       app.Model
- */
 class AppModel extends Model {
+  var $actsAs = array('Containable');
+
+  /**
+   * Never fetch any recursive data from associated models
+   * Use containable for any assocs
+   * @var integer
+   */
+  public $recursive = -1;
+
+  /**
+   * Check if two field values are equal (as in password confirmation)
+   * @param array $field check
+   * @param string $field2 as in 'User.password_confirm'
+   * return boolean
+   */
+  function isConfirmed($field, $field2) {
+    list($model,$field3) = (preg_split('/\./',$field2));
+    return(isset($this->data[$model][$field3]) && $this->data[$model][$field3] == current($field));
+  }
+
+  /**
+   * Indicates if a given string is a UUID
+   * @param string $str
+   * @return boolean
+   */
+  static function isUuid($str) {
+    return is_string($str) && preg_match('/^[A-Fa-f0-9]{8}-[A-Fa-f0-9]{4}-[A-Fa-f0-9]{4}-[A-Fa-f0-9]{4}-[A-Fa-f0-9]{12}$/', $str);
+  }
+
 }
