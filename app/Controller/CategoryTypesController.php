ae8ebb21729e249caa0010bdf20a2ed4fec8fb6b
Kevin <kevin@passbolt.com>
2012-07-29 17:11:22 +0530

    #PASSBOLT-121 : Creation of CategoryTypes Model and Controller

---
+++ app/Controller/CategoryTypesController.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Category Types controller
+ * This file will define how categories' types are managed
+ *
+ * @copyright    Copyright 2012, Passbolt.com
+ * @license      http://www.passbolt.com/license
+ * @package      app.Controller.CategoryTypesController
+ * @since        version 2.12.7
+ */
+class CategoryTypesController extends AppController {
+	
+}
---
+++ app/Model/CategoryType.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * CategoryType Model
+ *
+ * @copyright		 Copyright 2012, Passbolt.com
+ * @package			 app.Model.CategoryType
+ * @since				 version 2.12.7
+ * @license			 http://www.passbolt.com/license
+ */
+class CategoryType extends AppModel {
+	
+}
