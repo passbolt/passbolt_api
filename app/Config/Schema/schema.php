c9e651eddd6173c914f2e46af61bc46e13379ca9
Kevin <kevin@passbolt.com>
2012-08-05 23:20:31 +0530

    Anonymous => anonymous@passbolt.com

--- app/Config/Schema/schema.php
+++ app/Config/Schema/schema.php
@@ -36,7 +36,7 @@ class AppSchema extends CakeSchema {
 				case 'users':
 					$user = ClassRegistry::init('User');
 					$user->create();
-					$user->save(array('User'=>array('id' => 'bbd56042-c5cd-11e1-a0c5-080027796c4c', 'role_id' => '0208f3a4-c5cd-11e1-a0c5-080027796c4c', 'username' => 'Anonymous', 'password' => NULL, 'active' => 1, 'created' => '2012-07-04 13:45:11', 'modified' => '2012-07-04 13:45:14', 'created_by' => 'bbd56042-c5cd-11e1-a0c5-080027796c4c', 'modified_by' => 'bbd56042-c5cd-11e1-a0c5-080027796c4c')));
+					$user->save(array('User'=>array('id' => 'bbd56042-c5cd-11e1-a0c5-080027796c4c', 'role_id' => '0208f3a4-c5cd-11e1-a0c5-080027796c4c', 'username' => 'anonymous@passbolt.com', 'password' => NULL, 'active' => 1, 'created' => '2012-07-04 13:45:11', 'modified' => '2012-07-04 13:45:14', 'created_by' => 'bbd56042-c5cd-11e1-a0c5-080027796c4c', 'modified_by' => 'bbd56042-c5cd-11e1-a0c5-080027796c4c')));
 				break;
 				case 'roles':
 					$role = ClassRegistry::init('Role');
