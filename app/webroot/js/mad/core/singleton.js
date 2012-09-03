7fe90bb10f8d25ca02c99aae964a2153e10ab2a6
cedric <cedric@cedric-laptop>
2012-09-04 02:49:29 +0530

    #PASSBOLT-216 bug fix in the singleton test class

---
+++ app/webroot/js/mad/core/singleton.js
@@ -0,0 +1,76 @@
+steal(
+	'jquery/class'
+).then(function ($) {
+
+	/*
+	 * @class mad.core.Singleton
+	 * @inherits mad.core.Class
+	 * @parent mad.core
+	 * 
+	 * This class is our implementation of the pattern singleton on top of the
+	 * javascriptMVC framework. There are two ways to use the singleton class :
+	 * <ul>
+	 *	<li> 
+	 *		Either by <b>extending</b> directly the class
+	 *		@codestart
+	mad.core.Singleton.extend('myNewSingletonClass',{},{});
+	 *		@codeend
+	 *	</li>
+	 *	<li>
+	 *		Or by <b>augmenting</b> an existing class with the Singleton class
+	 *		@codestart
+	MyExistingClass.augment('mad.core.Singleton');
+	 *		@codeend
+	 *	</li>
+	 * </ul>
+	 * 
+	 * Once your class is a singleton, the only way to get the instance is to call
+	 * the static <i>singleton</i> method which expects the same parameters than the
+	 * constructor of the class.
+	 * 
+	 * @codestart
+	MyClass.singleton([CLASS_PARAMS]);
+	 * @codeend
+	 */
+	$.Class('mad.core.Singleton', /** @static */ {
+		/**
+		 * Singleton instance
+		 * @type {jQuery.Class}
+		 */
+		'singletonInstance': null,
+
+		/**
+		 * Get the singleton instance
+		 * @return {jQuery.Class}
+		 */
+		'singleton': function () {
+			var returnValue = null;
+
+			if(this.singletonInstance != null) {
+				returnValue = this.singletonInstance;
+			} else {
+				this.singletonInstance = 'CALL_FROM_SINGLETON';
+				returnValue = this.newInstance.apply(this, arguments);
+			}
+
+			return returnValue;
+		}
+	}, /** @hide @prototype */ {
+
+		/**
+		 * Class Constructor
+		 * @private
+		 */
+		'init': function () {
+			if (this.Class.fullName == 'mad.core.Singleton') {
+				throw new mad.error.CallAbstractFunction();
+			} else if (this.Class.singletonInstance != 'CALL_FROM_SINGLETON') {
+				throw new mad.error.CallPrivateFunction();
+			}
+
+			this.Class.singletonInstance = this;
+		}
+
+	});
+
+});
