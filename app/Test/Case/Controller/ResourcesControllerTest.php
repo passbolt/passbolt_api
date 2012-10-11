7767fe67e473e54dc1af222ae3cfcb640cd8b751
Kevin Muller <kevinmuller@Kevins-MacBook-Pro.local>
2012-10-11 02:57:14 +0200

    debug data

--- app/Test/Case/Controller/ResourcesControllerTest.php
+++ app/Test/Case/Controller/ResourcesControllerTest.php
@@ -86,11 +86,11 @@ class ResourcesControllerTest extends ControllerTestCase {
 
 		$url = "/resources/viewByCategory/" . $id . "/1.json";
 		$result = json_decode($this->testAction($url, array('return' => 'contents')), true);
+		$errors = print_r($result, true);
+		$this->log("debug : $errors", 'app.debug'); // Log the error
 		$this->assertEquals('washroom', $result['body'][3]['Resource']['name'],
 			$url . " test should read 'washroom' but is reading {$result['body'][3]['Resource']['name']}"
 		);
-		$errors = print_r($result, true);
-		$this->log("debug : $errors", 'app.debug'); // Log the error
 		$this->assertEquals(2, count($result['body'][1]['CategoryResource']),
 			$url . " counting the number of elements should return '2' but is reading " . count($result['body'][1]['CategoryResource'])
 		);
