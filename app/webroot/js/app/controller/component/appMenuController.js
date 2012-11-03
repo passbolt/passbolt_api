969401e20f6ff3956de262edc3521456edf9f852
cedric <cedric@cedric-laptop>
2012-11-04 02:22:33 +0530

    #PASSBOLT-289 Refactor app menu The contexual menu as already been comitted

--- app/webroot/js/app/controller/component/appMenuController.js
+++ app/webroot/js/app/controller/component/appMenuController.js
@@ -1,6 +1,6 @@
 steal(
-    MAD_ROOT + '/controller/component/menuController.js'
-).then(function ($) {
+    'mad/controller/component/menuController.js'
+).then(function () {
 
 	/*
 	 * @class passbolt.controller.component.AppMenuController
@@ -20,53 +20,41 @@ steal(
 
 	}, /** @prototype */ {
 
-		'init': function () {
-			this._super();
-			this.initMenuItems();
+		'init': function (el, options) {
+			this._super(el, options);
 		},
 
 		'initMenuItems': function () {
 			var menuItems = [
-				{ 'MenuItem': new mad.model.MenuItem({
+				new mad.model.Action({
 					'id': uuid(),
 					'label': 'home',
 					'cssClasses': ['home'],
 					'action': function () {
 						passbolt.eventBus.trigger('workspace_selected', 'js_passbolt_passwordWorkspace_controller');
 					}
-				})
-			}, { 'MenuItem': new mad.model.MenuItem({
+				}), new mad.model.Action({
 					'id': uuid(),
 					'label': 'passwords',
 					'action': function () {
 						passbolt.eventBus.trigger('workspace_selected', 'js_passbolt_passwordWorkspace_controller');
 					}
-				})
-			}, { 'MenuItem': new mad.model.MenuItem({
+				}), new mad.model.Action({
 					'id': uuid(),
 					'label': 'people',
 					'action': function () {
 						passbolt.eventBus.trigger('workspace_selected', 'js_passbolt_peopleWorkspace_controller');
 					}
-				})
-			}, { 'MenuItem': new mad.model.MenuItem({
+				}), new mad.model.Action({
 					'id': uuid(),
 					'label': 'help',
 					'action': function () {
 						passbolt.eventBus.trigger('workspace_selected', 'js_passbolt_passwordWorkspace_controller');
 					}
 				})
-			}];
+			];
 			this.load(menuItems);
 		}
-
-		/* ************************************************************** */
-		/* LISTEN TO THE VIEW EVENTS */
-		/* ************************************************************** */
-
-		/* ************************************************************** */
-		/* LISTEN TO THE APP EVENTS */
-		/* ************************************************************** */
 
 	});
 });
