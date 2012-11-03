7551fe7a489c694e725ab72fb3c696464c2affc2
cedric <cedric@cedric-laptop>
2012-11-04 02:33:47 +0530

    #PASSBOLT-273

--- app/webroot/js/stealconfig.js
+++ app/webroot/js/stealconfig.js
@@ -6,10 +6,14 @@ steal.config({
 			"lib/can/util/util.js": "lib/can/util/jquery/jquery.js",
 			"jquery/": "lib/jquerypp/",
 			"jquery": "lib/jquerypp/empty.js",
-			"funcunit": "lib/funcunit"
+			"funcunit": "lib/funcunit",
+			"mad": "lib/mad"
 		}
 	},
 	paths: {
+		// it will be used by our view class to fix a bug of the can view class which
+		// does not used the stealconfig file right now
+		"mad/": "lib/mad/"
 //		"jquery": "lib/jquery-1.8.2.min.js"
 	},
 	ext: {
