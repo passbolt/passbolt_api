bf825f29091fe0defb6c210f5e46f8f69c865995
Kevin Muller <kevinmuller@Kevins-MacBook-Pro.local>
2012-11-23 02:35:50 +0530

    empty models only for testing purpose. We will add everything inside later

---
+++ app/Model/Permission.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Permission  model
+ *
+ * @copyright    Copyright 2012, Passbolt.com
+ * @license      http://www.passbolt.com/license
+ * @package      app.Model.Permission
+ * @since        version 2.12.7
+ */
+App::uses('User', 'Model');
+
+class Permission extends AppModel {
+
+	public $actsAs = array('Containable', 'Trackable');
+
+
+}
