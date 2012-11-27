b54a27283d7213a3d4014bcadd9ab0f15706fc7f
Kevin Muller <kevinmuller@Kevins-MacBook-Pro.local>
2012-11-27 19:34:02 +0530

    PermissionDetail model

---
+++ app/Model/PermissionDetail.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * PermissionDetail Model
+ *
+ * @copyright		 Copyright 2012, Passbolt.com
+ * @license			 http://www.passbolt.com/license
+ * @package			 app.Model.PermissionDetail
+ * @since				 version 2.12.9
+ */
+class PermissionDetail extends AppModel {
+
+}
