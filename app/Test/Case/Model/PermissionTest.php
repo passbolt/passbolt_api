f455d71fa761333031b3a815abcef593198411cb
Kevin Muller <kevinmuller@Kevins-MacBook-Pro.local>
2012-11-30 21:04:42 +0530

    Unit tests for all 4 low level functions

--- app/Test/Case/Model/PermissionTest.php
+++ app/Test/Case/Model/PermissionTest.php
@@ -8,6 +8,7 @@
  * @license       http://www.passbolt.com/license
  */
 App::uses('Permission', 'Model');
+App::uses('PermissionType', 'Model');
 
 // TODO : stop using defaut schema and use fixtures instead
 class PermissionTest extends CakeTestCase {
@@ -35,17 +36,17 @@ class PermissionTest extends CakeTestCase {
 			array(
 				'aconame' => 'administration',
 				'groupname' => 'management',
-				'result' => '16'
+				'result' => PermissionType::ADMIN
 			),
 			array(
 				'aconame' => 'accounts',
 				'groupname' => 'management',
-				'result' => '16'
+				'result' => PermissionType::ADMIN
 			),
 			array(
 				'aconame' => 'cp-project1',
 				'groupname' => 'management',
-				'result' => '16'
+				'result' => PermissionType::ADMIN
 			),
 			array(
 				'aconame' => 'cp-project1',
@@ -55,7 +56,7 @@ class PermissionTest extends CakeTestCase {
 			array(
 				'aconame' => 'projects',
 				'groupname' => 'developers team leads',
-				'result' => '8'
+				'result' => PermissionType::UPDATE
 			)
 		);
 		foreach ($testcases as $testcase) {
@@ -66,7 +67,7 @@ class PermissionTest extends CakeTestCase {
 			$res = $this->Permission->query($query);
 			$permission = $this->Permission->findById($res[0][0]['permid']);
 			if($permission)
-				$permission = $permission['Permission']['permission_detail_id'];
+				$permission = $permission['Permission']['type'];
 			else
 				$permission = null;
 
@@ -82,7 +83,7 @@ class PermissionTest extends CakeTestCase {
 			array(
 				'aconame' => 'bank password',
 				'groupname' => 'management',
-				'result' => '16'
+				'result' => PermissionType::ADMIN
 			),
 			array(
 				'aconame' => 'salesforce account',
@@ -93,30 +94,128 @@ class PermissionTest extends CakeTestCase {
 			array(
 				'aconame' => 'salesforce account',
 				'groupname' => 'human resources',
-				'result' => '8'
+				'result' => PermissionType::UPDATE
 			),
 			// Test multi parents for resource
 			array(
 				'aconame' => 'shared resource',
 				'groupname' => 'company a',
-				'result' => '8'
+				'result' => PermissionType::UPDATE
 			)
 		);
 		foreach ($testcases as $testcase) {
 			$group = $this->Group->findByName($testcase['groupname']);
 			$resource = $this->Resource->findByName($testcase['aconame']);
-			//echo "<br/>===== trying with group {$group['Group']['name']} and category {$resource['Resource']['name']} grpid:{$group['Group']['id']} catid:{$resource['Resource']['id']} =====<br/>";
+			echo "<br/>===== trying with group {$group['Group']['name']} and category {$resource['Resource']['name']} grpid:{$group['Group']['id']} catid:{$resource['Resource']['id']} =====<br/>";
 			$query = "SELECT `passbolt`.getGroupResourcePermission('{$group['Group']['id']}', '{$resource['Resource']['id']}') AS permid";
 			$res = $this->Permission->query($query);
 			$permission = $this->Permission->findById($res[0][0]['permid']);
 			if($permission)
-				$permission = $permission['Permission']['permission_detail_id'];
+				$permission = $permission['Permission']['type'];
 			else
 				$permission = null;
 
 			$this->assertTrue(
 				$testcase['result'] == $permission,
 				"permissions for group {$group['Group']['name']} and resource {$resource['Resource']['name']} returned {$permission}
+				but should have returned {$testcase['result']}"
+			);
+		}
+	}
+
+	public function testMysqlFunctionGetUserCategoryPermission() {
+		$testcases = array(
+			array(
+				'aconame' => 'marketing',
+				'username' => 'dark.vador@test.com',
+				'result' => PermissionType::ADMIN
+			),
+			array(
+				'aconame' => 'd-project2',
+				'username' => 'cedric.alfonsi@test.com',
+				'result' => PermissionType::READ
+			),
+			array(
+				'aconame' => 'cp-project1',
+				'username' => 'remy.bertot@test.com',
+				'result' => PermissionType::ADMIN
+			),
+			array(
+				'aconame' => 'cp-project2',
+				'username' => 'remy.bertot@test.com',
+				'result' => PermissionType::UPDATE
+			),
+			// Test null
+			array(
+				'aconame' => 'marketing',
+				'username' => 'cedric.alfonsi@test.com',
+				'result' => null
+			)
+		);
+		foreach ($testcases as $testcase) {
+			$user = $this->User->findByUsername($testcase['username']);
+			$category = $this->Category->findByName($testcase['aconame']);
+			//echo "<br/>===== trying with group {$group['Group']['name']} and category {$resource['Resource']['name']} grpid:{$group['Group']['id']} catid:{$resource['Resource']['id']} =====<br/>";
+			$query = "SELECT `passbolt`.getUserCategoryPermission('{$user['User']['id']}', '{$category['Category']['id']}') AS permid";
+			$res = $this->Permission->query($query);
+			$permission = $this->Permission->findById($res[0][0]['permid']);
+			if($permission)
+				$permission = $permission['Permission']['type'];
+			else
+				$permission = null;
+
+			$this->assertTrue(
+				$testcase['result'] == $permission,
+				"permissions for user {$user['User']['username']} and category {$category['Category']['name']} returned {$permission}
+				but should have returned {$testcase['result']}"
+			);
+		}
+	}
+
+	public function testMysqlFunctionGetUserResourcePermission() {
+		$testcases = array(
+			array(
+				'aconame' => 'facebook account',
+				'username' => 'dark.vador@test.com',
+				'result' => PermissionType::ADMIN
+			),
+			array(
+				'aconame' => 'dp1-pwd1',
+				'username' => 'cedric.alfonsi@test.com',
+				'result' => PermissionType::READ
+			),
+			array(
+				'aconame' => 'cpp1-pwd1',
+				'username' => 'remy.bertot@test.com',
+				'result' => PermissionType::ADMIN
+			),
+			array(
+				'aconame' => 'cpp2-pwd1',
+				'username' => 'remy.bertot@test.com',
+				'result' => PermissionType::UPDATE
+			),
+			// Test null
+			array(
+				'aconame' => 'shared resource',
+				'username' => 'a-user1@test.com',
+				'result' => PermissionType::UPDATE
+			)
+		);
+		foreach ($testcases as $testcase) {
+			$user = $this->User->findByUsername($testcase['username']);
+			$resource = $this->Resource->findByName($testcase['aconame']);
+			//echo "<br/>===== trying with group {$group['Group']['name']} and category {$resource['Resource']['name']} grpid:{$group['Group']['id']} catid:{$resource['Resource']['id']} =====<br/>";
+			$query = "SELECT `passbolt`.getUserResourcePermission('{$user['User']['id']}', '{$resource['Resource']['id']}') AS permid";
+			$res = $this->Permission->query($query);
+			$permission = $this->Permission->findById($res[0][0]['permid']);
+			if($permission)
+				$permission = $permission['Permission']['type'];
+			else
+				$permission = null;
+
+			$this->assertTrue(
+				$testcase['result'] == $permission,
+				"permissions for user {$user['User']['username']} and resource {$resource['Resource']['name']} returned {$permission}
 				but should have returned {$testcase['result']}"
 			);
 		}
