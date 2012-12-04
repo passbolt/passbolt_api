5fab9fb153affcd6a06de87e830a1b793a7b6b65
Kevin Muller <kevinmuller@Kevins-MacBook-Pro.local>
2012-12-04 13:44:16 +0530

    #PASSBOLT-421

---
+++ app/Test/Case/Model/PermissionBenchmarkTest.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * Permission Benchmark Test
+ *
+ * @copyright     Copyright 2012, Passbolt.com
+ * @package       app.Test.Case.Model.PermissionBenchmarkTest
+ * @since         version 2.12.7
+ * @license       http://www.passbolt.com/license
+ */
+App::uses('Permission', 'Model');
+App::uses('PermissionType', 'Model');
+
+class PermissionBenchmarkTest extends CakeTestCase {
+
+	public $fixtures = array('app.resource', 'app.userPermission', 'app.role', 'app.group', 'app.groupUser', 'app.categoryType', 'app.dummyCategory', 'app.categoryResource', 'app.permissionType', 'app.permission');
+
+	public function setUp() {
+		parent::setUp();
+		$this->Permission = ClassRegistry::init('Permission');
+		$this->User = ClassRegistry::init('User');
+		$this->Role = ClassRegistry::init('Role');
+		$this->Category = ClassRegistry::init('Category');
+		$this->Resource = ClassRegistry::init('Resource');
+		$this->Group = ClassRegistry::init('Group');
+		$this->PermissionType = ClassRegistry::init('PermissionType');
+		$this->PermissionCache = ClassRegistry::init('PermissionCache');
+		$this->UserCategoryPermission = ClassRegistry::init('UserCategoryPermission');
+	}
+
+	public function microtimeFloat() {
+		list($usec, $sec) = explode(" ", microtime());
+		return ((float)$usec + (float)$sec);
+	}
+
+	public function testCompareViewsExecution() {
+		$user = $this->User->findByUsername('remy.bertot@test.com');
+
+		$this->Category->bindModel(array('hasOne' => array(
+			'PermissionCache', 'UserCategoryPermission'
+		)));
+		$this->UserCategoryPermission->bindModel(array('belongsTo' => array(
+			'Permission'
+		)));
+		$this->Permission->bindModel(array('belongsTo' => array(
+			'PermissionType'
+		)));
+		$timeStart = $this->microtimeFloat();
+		$cats  = $this->Category->find('all', array("order" => "Category.lft ASC", "contain" => array(
+			"PermissionCache" => array(
+				"foreignKey" => "aco_foreign_key",
+				"conditions" => array( "aco" => "Category", "aro" => "User", "aro_foreign_key" => $user['User']['id'])
+			)
+		)));
+		$timeEnd = $this->microtimeFloat();
+		$time1 = $timeEnd - $timeStart;
+
+		$timeStart = $this->microtimeFloat();
+		$cats  = $this->Category->find('all', array("order" => "Category.lft ASC", "contain" => array(
+			"UserCategoryPermission" => array(
+				"foreignKey" => "category_id",
+				"conditions" => array( "user_id" => $user['User']['id']),
+				"Permission" => array(
+					'foreignKey' => "permission_id",
+					'PermissionType' => array(
+						'foreignKey' => 'type'
+					)
+				)
+			)
+		)));
+		$timeEnd = $this->microtimeFloat();
+		$time2 = $timeEnd - $timeStart;
+
+		// Stupid test. We should find a way to display more relevant information in the test
+		$this->assertTrue($time2 < $time1);
+	}
+}
+
