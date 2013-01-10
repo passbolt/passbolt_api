37f2aced738ef19c2b6c7b931b39e37502b8cd0d
Kevin Muller <kevinmuller@Kevins-MacBook-Pro.local>
2013-01-10 16:22:57 +0530

    #PASSBOLT-446

--- app/Test/Case/Controller/UsersControllerTest.php
+++ app/Test/Case/Controller/UsersControllerTest.php
@@ -39,27 +39,34 @@ class UsersControllerTest extends ControllerTestCase {
 		// make sure there is no active session
 		$result = $this->testAction('/logout',array('return' => 'contents'), true);
 
-		// check with good user
-		/*
-		$data = array( 'User' => array(
-			'username' => 'test@passbolt.com',
-			'password' => 'password'
-		));
-		$result = $this->testAction('/users/login', array('return' => 'vars','method' => 'POST', 'data'=> $data), true);
-		pr($result); die;
-
 		// check if we get form
 		$result = $this->testAction('/users/login', array('return' => 'view','method' => 'GET'), true);
 		$this->assertEqual(preg_match('/(<form)/',$result), true,'/users/login with no data sent should return a form');
 
+		// check logging in with a good user
+		$data = array( 'User' => array(
+			'username' => 'test@passbolt.com',
+			'password' => 'password'
+		));
+
+		// Test that the user is returned properly in the session (authentication has done its job)
+		$result = $this->testAction('/users/login', array('return' => 'vars','method' => 'POST', 'data' => $data), true);
+		$this->assertEqual($this->user->get('User.username'), 'test@passbolt.com', "login test should have returned test@passbolt.com but has returned {$this->user->get('User.username')}");
+
+		// Test that the redirection is there as it should
+		$result = $this->testAction('/users/login', array('return' => 'view','method' => 'POST', 'data' => $data), true);
+		$this->assertEqual($this->headers['Location'], Router::url('/', true), "Login should have redirected to / but has not");
+
+		// Log out
+		$result = $this->testAction('/logout',array('return' => 'contents'), true);
+
 		// check with bogus user
 		$data = array( 'User' => array(
 			'username' => 'biloute@passbolt.com',
-			'password' => 'wow this is ridiculous'
+			'password' => 'ouaich mec'
 		));
-		$result = $this->testAction('/users/login', array('return' => 'vars','method' => 'POST', 'data' => $data), true);
-		$this->assertEqual($result['flashMessages'][0]['header']['status'], Message::ERROR, 'user login with bogus user should return an error');
-		*/
+		$result = $this->testAction('/users/login', array('return' => 'view','method' => 'POST', 'data' => $data), true);
+		$this->assertEqual(preg_match('/(<form)/',$result), true,'/users/login with wrong data should have returned a form');
 	}
 
 	public function testIndex() {
