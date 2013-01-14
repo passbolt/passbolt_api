176ae57a7d1a5c5f3f1384eedcf4b289423bd972
Kevin Muller <kevinmuller@Kevins-MacBook-Pro.local>
2013-01-14 17:58:41 +0530

    #PASSBOLT-480 table structure for authentication_blacklists

--- app/Config/Schema/schema.php
+++ app/Config/Schema/schema.php
@@ -280,4 +280,14 @@ class AppSchema extends CakeSchema {
 		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_unicode_ci', 'engine' => 'InnoDB')
 	);
 
+	public $authentication_blacklists = array(
+		'id' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 36, 'key' => 'primary', 'collate' => 'utf8_unicode_ci', 'charset' => 'utf8'),
+		'ip' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 33, 'collate' => 'utf8_unicode_ci', 'charset' => 'utf8'),
+		'expiry' => array('type' => 'datetime', 'null' => true, 'default' => null),
+		'created' => array('type' => 'datetime', 'null' => false, 'default' => null),
+		'modified' => array('type' => 'datetime', 'null' => false, 'default' => null),
+		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1)),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_unicode_ci', 'engine' => 'InnoDB')
+	);
+
 }
