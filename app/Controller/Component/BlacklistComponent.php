c8444c97ff6c92976f95284a2da5b15473c6d33b
Kevin Muller <kevinmuller@Kevins-MacBook-Pro.local>
2013-01-17 01:21:36 +0530

    #PASSBOLT-480

---
+++ app/Controller/Component/BlacklistComponent.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Blacklist Component
+ * Manages blacklisting of ip addresses in the system. 
+ *
+ * @copyright		 copyright 2012 Passbolt.com
+ * @package			 app.Controller.BlacklistComponent
+ * @since				 version 2.13.03
+ * @license			 http://www.passbolt.com/license
+ */
+class BlacklistComponent extends AuthComponent {
+
+/**
+ * stores ip of user for current authentication
+ */
+	public $ip = null;
+
+/**
+ * startup function
+ * @param Controller $controller. the calling controller
+ */
+	public function startup(&$controller) {
+		$this->controller = $controller;
+		$this->AuthenticationLog = ClassRegistry::init('AuthenticationLog');
+		$this->AuthenticationBlacklist = ClassRegistry::init('AuthenticationBlacklist');
+
+		$this->ip = $controller->request->clientIp();
+
+		// If address is blacklisted, doesn't return anything and stops all operations
+		if ($this->isBlacklist()) {
+			exit(0);
+		}
+
+		return parent::startup($controller);
+	}
+
+/**
+ * detect whether the current address is blacklisted
+ */
+	public function isBlacklist() {
+		$bls = $this->AuthenticationBlacklist->find('all', array(
+			'conditions' => array(
+				'expiry >' => date('Y-m-d H:i:s')
+			)
+		));
+		foreach ($bls as $bl) {
+			if ($this->controller->IpAddress->inRange($this->ip, $bl['AuthenticationBlacklist']['ip'])) {
+				return true;
+			}
+		}
+		return false;
+	}
+}
+
+ 
