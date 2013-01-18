47040f737396021d0096ff6f515679acbc1c494b
cedric <cedric@cedric-laptop>
2013-01-18 10:54:05 +0530

    #PASSBOLT-447 - Almost finish the addAcoPermission action (it rest to test unique combinaison)

--- app/Controller/PermissionsController.php
+++ app/Controller/PermissionsController.php
@@ -10,119 +10,182 @@
  */
 
 App::uses('Permission', 'Model');
+App::uses('PermissionType', 'Model');
  
 class PermissionsController extends AppController  {
 
+	public function __construct($request = NULL, $response = NULL) {
+		parent::__construct($request, $response);
+		$this->PermissionType = ClassRegistry::init('PermissionType');
+	}
+
 /**
- * Add permission on an instance
- * @param string model the model of the instance
- * @param uuid id the id of the target instance
+ * Add permission to a target instance of a given model
+ * @param string acoModelName the model of the target aco model instance
+ * @param uuid acoInstanceId the uuid of the target aco model instance
  * @return array
  */
-	public function addAcoPermissions($model = '', $id = null) {
-		// camelize the model parameter
-		$model = Inflector::camelize($model);
-		// The model to use relative to the given model string
-		$Model = null;
-		// The instance relative to the given id string
-		$instance = null;
+	public function addAcoPermissions($acoModelName = '', $acoInstanceId = null) {
+		// The target ACO Model to use
+		$acoModel = null;
+		// The target instance to add permissions on
+		$acoInstance = null;
+		// The target ARO Model name to use
+		$aroModelName = null;
+		// The target ARO Model to use
+		$aroModel = null;
+		// The target instance to add permissions on
+		$aroInstance = null;
+		// The given permission type
+		$permissionType = isset($this->request->data['Permission']['type']) ? $this->request->data['Permission']['type'] : null; 
 
-		// check if the target model is permissionable
-		if(!in_array($model, Permission::getAllowedAcos())) {
-			$this->Message->error(__('The model is not permissionable'));
-			return;
-		}
-		// load the ACO relative model
-		$Model = ClassRegistry::init($model);
-		
-		// no instance id given
-		if(is_null($id)) {
-			$this->Message->error(__('The id is missing'));
+		// check if the target ACO model is permissionable
+		if(!$this->Permission->isValidAco($acoModelName)) {
+			$this->Message->error(__('The model (%s) is not permissionable', $acoModelName));
 			return;
 		}
 		
-		// the id is invalid
-		if (!Common::isUuid($id)) {
-			$this->Message->error(__('The id is invalid'));
+		// no aco instance id given
+		if(is_null($acoInstanceId)) {
+			$this->Message->error(__('The ACO instance id parameter is missing'));
 			return;
 		}
 		
-		// the instance does not exist
-		$instance = $Model->findById($id);
-		if (!$instance) {
-			$this->Message->error(__('The instance doesn\'t exist'));
+		// the aco instance id is invalid
+		if (!Common::isUuid($acoInstanceId)) {
+			$this->Message->error(__('The id (%s) is invalid', $acoInstanceId));
 			return;
 		}
 		
+		// the aco instance instance does not exist
+		if (!$this->Permission->isInstanceExisting($acoInstanceId, $acoModelName)) {
+			$this->Message->error(__('The ACO instance (%s) for the model (%s) doesn\'t exist or the user is not allowed to access it', $acoInstanceId, $acoModelName));
+			return;
+		}
 		
+		// Treat the posted data
+		// Get the target ARO model and instance id
+		$dataAroModelNames = array_keys($this->request->data);
+		foreach($dataAroModelNames as $dataAroModelName) {
+			// if the current data key is an allowed ARO model
+			if($this->Permission->isValidAro($dataAroModelName)) {
+				$aroModelName = $dataAroModelName;
+				if(isset($this->request->data[$aroModelName]['id'])) {
+					$aroInstanceId = $this->request->data[$aroModelName]['id'];
+				}
+				break;
+			}
+		}
 		
+		// no ARO instance id given
+		if(is_null($aroInstanceId)) {
+			$this->Message->error(__('The ARO id is missing'));
+			return;
+		}
+		
+		// the ARO instance id is invalid
+		if (!Common::isUuid($aroInstanceId)) {
+			$this->Message->error(__('The id (%s) is invalid', $id));
+			return;
+		}		
+		// the ARO instance does not exist
+		if (!$this->Permission->isInstanceExisting($aroInstanceId, $aroModelName)) {
+			$this->Message->error(__('The ARO instance (%s) for the model (%s) doesn\'t exist or the user is not allowed to access it', $aroInstanceId, $aroModelName));
+			return;
+		}
+		
+		// Check the given permission
+		if(is_null($permissionType)) {
+			$this->Message->error(__('No permission type given'));
+			return;
+		}
+		
+		// Valid permission type
+		if(!$this->PermissionType->isValidSerial($permissionType)) {
+			$this->Message->error(__('The given permission type is not valid'));
+			return;
+		}
+		
+		// add the new permission
+		$data = array('Permission'=>array(
+			'aco' => $acoModelName,
+			'aco_foreign_key' => $acoInstanceId,
+			'aro' => $aroModelName,
+			'aro_foreign_key' => $aroInstanceId,
+			'type' => $permissionType
+		));
+		$this->Permission->create();
+		$this->Permission->set($data);
+		if(!$this->Permission->validates()){
+			$this->Message->error($this->Permission->validationErrors);
+			return;
+		}
+		
+		$permission = $this->Permission->save($data);		
 		$this->Message->success();
 	}
 
 /**
  * View applied permissions on an instance
- * @param string model the model of the instance
- * @param uuid id the id of the target instance
+ * @param string acoModelName the model of the target aco model instance
+ * @param uuid acoInstanceId the uuid of the target aco model instance
  * @return array
  */
-	public function viewAcoPermissions($model = null, $id = null) {
+	public function viewAcoPermissions($acoModelName = '', $acoInstanceId = null) {
 		$returnValue = array();
-		// camelize the model parameter
-		$model = Inflector::camelize($model);
-		// The model to use relative to the given model string
-		$Model = null;
-		// The instance relative to the given id string
-		$instance = null;
+		// The target ACO Model to use
+		$acoModel = null;
+		// The target instance to add permissions on
+		$acoInstance = null;
 
-		// check if the target model is permissionable
-		if(!in_array($model, Permission::getAllowedAcos())) {
-			$this->Message->error(__('The model is not permissionable'));
+		// check if the target ACO model is permissionable
+		if(!$this->Permission->isValidAco($acoModelName)) {
+			$this->Message->error(__('The model (%s) is not permissionable', $acoModelName));
+			return;
+		}
+				
+		// no aco instance id given
+		if(is_null($acoInstanceId)) {
+			$this->Message->error(__('The ACO instance id parameter is missing'));
 			return;
 		}
 		
-		$Model = ClassRegistry::init($model);
-		
-		// no instance id given
-		if(is_null($id)) {
-			$this->Message->error(__('The id is missing'));
+		// the aco instance id is invalid
+		if (!Common::isUuid($acoInstanceId)) {
+			$this->Message->error(__('The id (%s) is invalid', $acoInstanceId));
 			return;
 		}
 		
-		// the id is invalid
-		if (!Common::isUuid($id)) {
-			$this->Message->error(__('The id is invalid'));
+		// the aco instance instance does not exist
+		if (!$this->Permission->isInstanceExisting($acoInstanceId, $acoModelName)) {
+			$this->Message->error(__('The ACO instance (%s) for the model (%s) doesn\'t exist or the user is not allowed to access it', $acoInstanceId, $acoModelName));
 			return;
 		}
 		
-		// the instance does not exist
-		$instance = $Model->findById($id);
-		if (!$instance) {
-			$this->Message->error(__('The instance doesn\'t exist'));
-			return;
-		}
+		// case used by find options functions
+		// this view case has to be defined in model which represent the views
+		$viewCase = 'viewBy' . $acoModelName;
 		
-		// case used by find options function
-		$viewCase = 'viewBy' . $model;
-		
-		// get user's permissions for the target instance
-		$viewName = 'User' . $model . 'Permission';
+		// get user's permissions for the target instance 
+		// @todo We need a strong use case to check this part. Think about the case, direct user permission on the parent categories !!!  
+		$viewName = 'User' . $acoModelName . 'Permission';
 		$ModelView = ClassRegistry::init($viewName);
-		$foreignKey = strtolower($model) . '_id';
+		$foreignKey = Inflector::underscore($acoModelName) . '_id';
 		$upData = array(
 			$viewName => array(
-				$foreignKey => $id
+				$foreignKey => $acoInstanceId
 			)
 		);
 		$upOptions = $ModelView->getFindOptions($viewCase, User::get('Role.name'), $upData);
 		$ups = $ModelView->find('all', $upOptions);
 		
 		// get group's permissions for the target instance
-		$viewName = 'Group' . $model . 'Permission';
+		$viewName = 'Group' . $acoModelName . 'Permission';
 		$ModelView = ClassRegistry::init($viewName);
-		$foreignKey = strtolower($model) . '_id';
+		$foreignKey = strtolower($acoModelName) . '_id';
 		$gpData = array(
 			$viewName => array(
-				$foreignKey => $id
+				$foreignKey => $acoInstanceId
 			)
 		);
 		$gpOptions = $ModelView->getFindOptions($viewCase, User::get('Role.name'), $gpData);
