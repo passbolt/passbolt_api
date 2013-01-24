4cf00609607a0dd7d63cee7eace521f250fcc05e
cedric <cedric@cedric-laptop>
2013-01-24 13:03:06 +0530

    #PASSBOLT-447

--- app/Model/Behavior/PermissionableBehavior.php
+++ app/Model/Behavior/PermissionableBehavior.php
@@ -9,6 +9,7 @@
  */
 
 App::uses('Permission', 'Model');
+App::uses('PermissionType', 'Model');
 
 class PermissionableBehavior extends ModelBehavior {
 
@@ -19,32 +20,38 @@ class PermissionableBehavior extends ModelBehavior {
  * The permissionnable before find method is used to augment the query to find reccords 
  * functions of the User and the User[AroModelName]Permission model
  */
-	public function beforeFind($Model, $queryData = array()) {
+	public function beforeFind(&$model, $queryData = array()) {
 		if (User::get('Role.name') == Role::USER ||
-						User::get('Role.name') == Role::GUEST) {
+			User::get('Role.name') == Role::GUEST) {
+			
+			$userPermissionModelName = 'User' . $model->alias . 'Permission'; // ex : UserCategoryPermission, UserResourcePermission
+			$foreignModelPrimaryKey = Inflector::underscore($model->alias) . '_id'; // ex : category_id
 			
 			// Permissionable find request options
 			$permOptions = array(
-				'fields' => array('UserCategoryPermission.permission_id', 'UserCategoryPermission.permission_type'),
-				'conditions' =>	 array(
-					'UserCategoryPermission.user_id' => User::get('id'),
-					'UserCategoryPermission.permission_type >' => PermissionType::DENY		
+				'fields' => array(
+					$userPermissionModelName . '.permission_id',
+					$userPermissionModelName . '.permission_type'
 				),
-				'contain' => array('UserCategoryPermission')
+				'conditions' =>	 array(
+					$userPermissionModelName . '.user_id' => User::get('id'),
+					$userPermissionModelName . '.permission_type >' => PermissionType::DENY		
+				),
+				'contain' => array($userPermissionModelName)
 			);
 
 			// Bind the target model to the permissions models
-			$Model->bindModel(array('hasOne'=>array(
+			$model->bindModel(array('hasOne'=>array(
 				// @todo automatically bind the [AroModelName][targetAcoModelName]Permission model to the model
-				'UserCategoryPermission' => array(
-					'foreignKey' => 'category_id'
+				$userPermissionModelName => array(
+					'foreignKey' => $foreignModelPrimaryKey
 				),
 				'GroupCategoryPermission' => array(
-					'foreignKey' => 'resource_id'
+					'foreignKey' => $foreignModelPrimaryKey
 				),
 				'Permission'=> array(
 					'foreignKey' => false,
-					'conditions' => array(' Permission.id = UserCategoryPermission.permission_id '),
+					'conditions' => array('Permission.id' => $userPermissionModelName . '.permission_id '),
 					'type' => 'LEFT'
 				))), false
 			);
@@ -71,11 +78,11 @@ class PermissionableBehavior extends ModelBehavior {
  * Details of after find method
  * @link http://api20.cakephp.org/class/model#method-ModelafterFind
  */
-	public function afterFind($Model, $results, $primary = false) {
+	public function afterFind(&$model, $results, $primary = false) {
 		if (User::get('Role.name') == Role::USER ||
-						User::get('Role.name') == Role::GUEST) {
+			User::get('Role.name') == Role::GUEST) {
 			
-			$Model->unbindModel(array('hasOne'=>array('UserCategoryPermission', 'Permission', 'GroupCategoryPermission')), false);
+			$model->unbindModel(array('hasOne'=>array('UserCategoryPermission', 'Permission', 'GroupCategoryPermission')), false);
 		}
 		return $results;
 	}
@@ -87,24 +94,47 @@ class PermissionableBehavior extends ModelBehavior {
  * The permissionnable after save method is used to automatically give to the user
  * the ADMIN right to the reccords he has just inserted
  */
-	public function afterSave($Model, $created) {
-		// make the creator administrator of the created instance
-		if (User::get('Role.name') == Role::USER) {
-			$data = array('Permission'=>array(
-				'aco' => $this->alias,
-				'aco_foreign_key' => $this->id,
-				'aro' => 'User',
-				'aro_foreign_key' => User::get('User.id'),
-				'type' => PermissionType::ADMIN
-			));
-			$this->Permission->create();
-			$this->Permission->set($data);
-			if(!$this->Permission->validates()){
-				$this->Message->error($this->Permission->validationErrors);
-				return;
-			}
-			
-			$permission = $this->Permission->save($data);				
+	public function afterSave(&$model, $created) {
+		if($created) {
+			$Permission = Common::getModel('Permission');
+			// make the creator administrator of the created instance
+			if (User::get('Role.name') == Role::USER) {
+				$data = array('Permission'=>array(
+					'aco' => $model->alias,
+					'aco_foreign_key' => $model->id,
+					'aro' => 'User',
+					'aro_foreign_key' => User::get('User.id'),
+					'type' => PermissionType::ADMIN
+				));
+				$Permission->create();
+				$Permission->set($data);
+				if(!$Permission->validates()){
+					$this->Message->error($Permission->validationErrors);
+					return;
+				}
+				
+				$permission = $Permission->save($data);				
+			}	
 		}
+	}
+	
+/**
+ * Check a user is authorized to allowed to access a reccord
+ * @param uuid id Id of the record
+ * @param integer permissionType The permission type
+ * @return bool
+ */
+	public function isAuthorized(&$model, $id, $permissionType = PermissionType::READ) {
+		$userPermissionModelName = 'User' . $model->alias . 'Permission';
+		$UserPermission = Common::getModel($userPermissionModelName);
+		$findOptions = array(
+			'fields' => array('permission_id', 'permission_type'),
+			'conditions' =>	 array(
+				$userPermissionModelName . '.user_id' => User::get('id'),
+				$userPermissionModelName . '.permission_type >=' => $permissionType		
+			)
+		);
+		$result = $UserPermission->find('first', $findOptions);
+		return !empty($result) ? true : false;
 	}
 }
