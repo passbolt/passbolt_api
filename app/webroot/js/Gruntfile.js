865786ca464df3655ab12e7bf5b0e7c22eff8c01
cedric <cedric@cedric-laptop.(none)>
2013-05-05 17:38:32 +0530

    Add a new jobs to autmatise saucelabs test Reduce the number of test in our unit tests suite for the manual testing/

--- app/webroot/js/Gruntfile.js
+++ app/webroot/js/Gruntfile.js
@@ -1,18 +1,58 @@
 module.exports = function (grunt) {
 
   grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-qunit-junit');
+  grunt.loadNpmTasks('grunt-contrib-qunit');
+  grunt.loadNpmTasks('grunt-saucelabs');
 
   grunt.initConfig({
     connect: {
       server: {
         options: {
-          port: 8000,
-          base: '.',
+          port: 80,
+          base: 'js',
           keepalive: true
         }
       }
-    }
+    },
+	'qunit': {
+		all: {
+			options: {
+		        urls: ['http://passbolt.local/js/lib/mad/funcunit.html']
+			}
+		}
+    },
+	'saucelabs-qunit': {
+		all: {
+			options: {
+				username: 'cedricalfonsi',
+				key: '82c85764-33b5-4ed7-ae2f-ae4e74268d10',
+				tags: ['master'],
+				testTimeout:1000000,
+				testReadyTimeout:100000,
+				detailedError:true,
+				urls: ['http://passbolt.local/js/lib/mad/funcunit.html'],
+				browsers: [{
+					browserName: 'chrome'
+				}]
+			},
+			onTestComplete: function(){
+		        // Called after a qunit unit is done, per page, per browser
+		        // Return true or false, passes or fails the test
+		        // Returning undefined does not alter the test result
+		
+		        // For async return, call
+		        var done = this.async();
+		        setTimeout(function(){
+		          // Return to this test after 1000 milliseconds
+		          done(/*true or false changes the test result, undefined does not alter the result*/);
+		        }, 1000);
+		    }
+		}
+	},
   })
 
-  grunt.registerTask("server", "connect:server")
+  // grunt.registerTask("server", "connect:server")
+  grunt.registerTask('test', ['connect', 'qunit_junit', 'qunit']);
+  grunt.registerTask('remote-test', ['saucelabs-qunit']);
 };
