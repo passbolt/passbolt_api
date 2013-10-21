63b9d52de0ff3e7a4b3d84c4fe1fa1c44e98567b
Kevin <kevin@enova-tech.net>
2013-10-21 11:57:41 +0530

    #PASSBOLT-658 code cleanup

--- app/webroot/js/app/view/component/sidebarSection/sidebarSectionTags.js
+++ app/webroot/js/app/view/component/sidebarSection/sidebarSectionTags.js
@@ -5,8 +5,8 @@ steal(
 ).then(function () {
 
 		/*
-		 * @class passbolt.view.component.Comments
-		 * @inherits mad.view.View
+		 * @class passbolt.view.component.SidebarSection.SidebarSectionTags
+		 * @inherits passbolt.view.component.SidebarSection
 		 */
 		passbolt.view.component.SidebarSection.extend('passbolt.view.component.SidebarSection.SidebarSectionTags', /** @static */ {
 
