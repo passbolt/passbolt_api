79148ccf8e9f8cb5bddc60f57c1e1672913089ad
cedricalfonsi <cedricalfonsi@gmail.com>
2014-05-04 21:44:38 +0530

    PASSBOLT-600 Patch the Fixture task to add the missing semicolon which break the fixtures.

--- app/Console/Command/Task/FixtureTask.php
+++ app/Console/Command/Task/FixtureTask.php
@@ -269,6 +269,8 @@ class FixtureTask extends BakeTask {
 	public function generateFixtureFile($model, $otherVars) {
 		$defaults = array('table' => null, 'schema' => null, 'records' => null, 'import' => null, 'fields' => null);
 		$vars = array_merge($defaults, $otherVars);
+		// Dirty patch to add missing semicolon.
+		$vars['schema'] = $vars['schema'] . ';';
 
 		$path = $this->getPath();
 		$filename = Inflector::camelize($model) . 'Fixture.php';
