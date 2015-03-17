e5ac959d3b1f7337fab07cd957f2c9fff8d6a5f1
Kevin <kevin@enova-tech.net>
2015-03-17 12:59:57 +0530

    #PASSBOLT-849 removed kpnlabs/gaufrette from vendor

--- app/Vendor/knplabs/gaufrette/tests/Gaufrette/Functional/Adapter/AmazonS3Test.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace Gaufrette\Functional\Adapter;
-
-class AmazonS3Test extends FunctionalTestCase
-{
-}
--- app/Vendor/knplabs/gaufrette/tests/Gaufrette/Functional/Adapter/SftpTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Gaufrette\Functional\Adapter;
-
-use Gaufrette\Adapter\Sftp;
-
-class SftpTest extends FunctionalTestCase
-{
-}
