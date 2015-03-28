5c76ac9539a0b6ffee0446b5ef82608925cb0cbf
Kevin <kevin@enova-tech.net>
2015-03-28 20:48:47 +0530

    #PASSBOLT-876 component that will contain entry points to send notifications

---
+++ app/Controller/Component/EmailNotificatorComponent.php
@@ -0,0 +1,111 @@
+<?php
+/**
+ * EmailNotification Component
+ * This class offers tools to send notification emails.
+ *
+ * @copyright    Copyright 2012, Passbolt.com
+ * @license      http://www.passbolt.com/license
+ * @package      app.Controller.EmailNotificationComponent
+ * @since        version 2.12.7
+ */
+
+class EmailNotificatorComponent extends Component {
+
+	/**
+	 * Initialize
+	 */
+	public function initialize(Controller $controller, $settings = array()) {
+		$this->Controller = $controller;
+		$this->Permission = Common::getModel('Permission');
+		$this->User = Common::getModel('User');
+		$this->Resource = Common::getModel('Resource');
+		$this->Secret = Common::getModel('Secret');
+		$this->EmailNotification = Common::getModel('EmailNotification');
+		parent::initialize($controller);
+	}
+
+	/**
+	 * Send a notification email regarding a new password that has been shared with the user.
+	 *
+	 * @param uuid $toUserId
+	 *   user id of the recipient
+	 * @param array $data
+	 *   variables to pass to the template which should contain
+	 *     resource_id the resource id
+	 *     sharer_id the user who is sharing the resource
+	 */
+	public function passwordSharedNotification($toUserId, $data) {
+
+		// get resource.
+		$resource = $this->Resource->find(
+			'first',
+			array(
+				'conditions' => array(
+					'Resource.id' =>$data['resource_id']
+				),
+				'fields' => array(
+					'Resource.name',
+					'Resource.username',
+					'Resource.uri',
+					'Resource.description'
+				),
+				'contain' => array(
+					'Secret' => array(
+						'fields' => array(
+							'Secret.data',
+							'Secret.modified',
+						),
+						'conditions' => array(
+							'Secret.user_id' => $toUserId
+						),
+					)
+				)
+			)
+		);
+
+		$recipient = $this->User->find(
+			'first',
+			array(
+				'conditions' => array(
+					'User.id' => $toUserId
+				),
+			)
+		);
+
+		$sharer = $this->User->find(
+			'first',
+			array(
+				'conditions' => array(
+					'User.id' => $data['sharer_id']
+				),
+				'fields' => array(
+					'username'
+				),
+				'contain' => array(
+					'Profile' => array(
+						'fields' => array(
+							'Profile.first_name',
+							'Profile.last_name',
+						),
+						'Avatar' => array(
+							'fields' => array(
+								'Avatar.*'
+							)
+						)
+					),
+				)
+			)
+		);
+
+		// Send notification.
+		$this->EmailNotification->send(
+			$recipient['User']['username'],
+			__("%s shared %s with you", $sharer['Profile']['first_name'], $resource['Resource']['name']),
+			array(
+				'sender' => $sharer,
+				'resource' => $resource,
+			),
+			'new_password_share'
+		);
+	}
+}
