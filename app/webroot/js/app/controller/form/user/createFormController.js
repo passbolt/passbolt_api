ef7826b006d22ed9aa425282e14f02a18ea0e4ee
Kevin <kevin@enova-tech.net>
2015-04-17 18:14:12 +0530

    #PASSBOLT-905 add user form

--- app/webroot/js/app/controller/form/user/createFormController.js
+++ app/webroot/js/app/controller/form/user/createFormController.js
@@ -21,6 +21,7 @@ steal(
 			'defaults': {
 				'templateBased': true,
 				'passwordField': null,
+				'currentPasswordField': null,
 				// @todo should be dynamic functions of creation or update
 				'action': 'create'
 			}
@@ -57,17 +58,39 @@ steal(
 					}).start(),
 					new mad.form.FeedbackController($('#js_field_last_name_feedback'), {}).start()
 				);
-				// Add resource username field
+				// Role box.
+				// Build roles data.
+				var roles = {};
+				roles[cakephpConfig.roles.admin] = __('This user is an administrator');
+				roles[cakephpConfig.roles.user] = __('This user is a normal user');
+				// Role component.
+				this.options.role = new mad.form.element.CheckboxController($('#js_field_role_id'), {
+						'name': 'role_id',
+						'modelReference': 'passbolt.model.User.role_id',
+						'availableValues': roles
+					}
+				).start();
+				// Add role element to form.
 				this.addElement(
-					new mad.form.element.TextboxController($('#js_field_username'), {
-						modelReference: 'passbolt.model.User.username'
-					}).start(),
-					new mad.form.FeedbackController($('#js_field_username_feedback'), {}).start()
+					this.options.role,
+					new mad.form.FeedbackController($('#js_field_role_id_feedback'), {}).start()
 				);
+				// Hide everything that is not admin.
+				$('input[type=checkbox]', $('#js_field_role_id')).not("[value='" + cakephpConfig.roles.admin + "']").hide().next('span').hide();
+
+				if (this.options.action == 'create') {
+					// Add resource username field
+					this.addElement(
+						new mad.form.element.TextboxController($('#js_field_username'), {
+							modelReference: 'passbolt.model.User.username'
+						}).start(),
+						new mad.form.FeedbackController($('#js_field_username_feedback'), {}).start()
+					);
+				}
 
 				// Add secret data field
 				// Only while creating a new user
-				if (this.options.action == 'create') {
+				if (this.options.action != 'create') {
 					this.options.passwordField = new mad.form.element.TextboxController($('#js_field_password'), {
 						modelReference: 'passbolt.model.User.password'
 					}).start();
@@ -79,6 +102,21 @@ steal(
 						}).start()
 					);
 
+					// Declare current password field.
+					this.options.currentPasswordField = new mad.form.element.TextboxController($('#js_field_current_password'), {
+						modelReference: 'passbolt.model.User.current_password'
+					}).start();
+					this.addElement(this.options.currentPasswordField);
+					// Button to see clear current password.
+					this.options.currentPasswordClear = this.addElement(
+						new mad.form.element.TextboxController($('#js_field_current_password_clear'), {
+							'state': 'hidden'
+						}).start()
+					);
+					// Show/Hide the password
+					this.options.showCurrPwdButton = new mad.controller.component.ButtonController($('#js_show_curr_pwd_button'))
+						.start();
+
 					// Show/Hide the password
 					this.options.showPwdButton = new mad.controller.component.ButtonController($('#js_show_pwd_button'))
 						.start();
@@ -86,6 +124,7 @@ steal(
 					// generate a password
 					this.options.genPwdButton = new mad.controller.component.ButtonController($('#js_gen_pwd_button'))
 						.start();
+
 
 					// The secret strength component
 					var secret = can.getObject('data.Secret.data', this.options);
@@ -139,6 +178,29 @@ steal(
 			},
 
 			/**
+			 * Observe when a role is checked. and Unselect the other one.
+			 * @param el
+			 * @param ev
+			 * @param roleId
+			 */
+			'{role} checked': function(el, ev, roleId) {
+				// Force only one value.
+				this.options.role.setValue(roleId);
+			},
+
+			/**
+			 * Observe when a role is changed. If no role is selected, select user as default.
+			 * @param el
+			 * @param ev
+			 * @param val
+			 */
+			'{role} changed': function(el, ev, val) {
+				if (val.value.length == 0) {
+					this.options.role.setValue(cakephpConfig.roles.user);
+				}
+			},
+
+			/**
 			 * Observe when the user wants to see the password unscrumbled
 			 * @param {HTMLElement} el The element the event occured on
 			 * @param {HTMLEvent} ev The event which occured
@@ -165,6 +227,37 @@ steal(
 					passwordClear.setValue(password.getValue());
 					// push the button
 					this.options.showPwdButton.view.addClass('selected');
+				}
+			},
+
+			/**
+			 * Observe when the user wants to see the password unscrumbled
+			 * @param {HTMLElement} el The element the event occured on
+			 * @param {HTMLEvent} ev The event which occured
+			 * @return {void}
+			 */
+			'{showCurrPwdButton} click': function(el, ev) {
+				var password = this.getElement('js_field_current_password');
+				var passwordClear = this.getElement('js_field_current_password_clear');
+
+
+				// if the password is already hidden
+				if (password.state.is('hidden')) {
+					// hide the unscrambled password
+					passwordClear.setState('hidden');
+					// show the password field
+					password.setState('ready');
+					// unpush the button
+					this.options.showCurrPwdButton.view.removeClass('selected');
+				}
+				else {
+					// hide the password field
+					password.setState('hidden');
+					// display the unscrambled password
+					passwordClear.setState('ready');
+					passwordClear.setValue(password.getValue());
+					// push the button
+					this.options.showCurrPwdButton.view.addClass('selected');
 				}
 			},
 
