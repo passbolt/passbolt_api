9b89ade370873ef06782b6a8aee098df4db18253
Kevin <kevin@enova-tech.net>
2015-04-22 12:48:26 +0530

    #PASSBOLT-360 added validation rules for authenticationToken

--- app/Model/AuthenticationToken.php
+++ app/Model/AuthenticationToken.php
@@ -14,9 +14,92 @@
 class AuthenticationToken extends AppModel {
 
 	/**
+	 * Details of belongs to relationships
+	 *
+	 * @var array
+	 * @link http://book.cakephp.org/2.0/en/models/associations-linking-models-together.html#
+	 */
+	public $belongsTo = array(
+		'User',
+	);
+
+	/**
 	 * Length of the base random string used to generate tokens.
 	 */
 	const TOKEN_STRING_LENGTH = 30;
+
+	/**
+	 * Get the validation rules upon context
+	 *
+	 * @param string $case (optional) The target validation case if any.
+	 * @return array cakephp validation rules
+	 */
+	public static function getValidationRules($case = 'default') {
+		$default = array(
+			'id' => array(
+				'uuid' => array(
+					'rule' => 'uuid',
+					'message' => __('UUID must be in correct format')
+				)
+			),
+			'token' => array(
+				'validMD5' => array(
+					'rule' => array('checkValidMd5', true),
+					'required' => true,
+					'allowEmpty' => false,
+					'message' => __('Token has an invalid format')
+				)
+			),
+			'user_id' => array(
+				'uuid' => array(
+					'rule' => 'uuid',
+					'required' => true,
+					'allowEmpty' => false,
+					'message'	=> __('UUID must be in correct format')
+				),
+				'exist' => array(
+					'rule' => array('userExists', null),
+					'message' => __('The user id provided does not exist')
+				),
+			),
+		);
+		switch ($case) {
+			default:
+			case 'default':
+				$rules = $default;
+				break;
+		}
+		return $rules;
+	}
+
+	/**
+	 * Check validation rule, whether a md5 is valid.
+	 * @param $check
+	 * @return bool
+	 */
+	public function checkValidMd5($check) {
+		if ($check['token'] == null) {
+			return false;
+		} else {
+			return strlen($check['token']) == 32 && ctype_xdigit($check['token']);
+		}
+	}
+
+	/**
+	 * Check if a user with same id exists
+	 * @param $check
+	 * @return bool
+	 */
+	public function userExists($check) {
+		if ($check['user_id'] == null) {
+			return false;
+		} else {
+			$exists = $this->User->find('count', array(
+					'conditions' => array('User.id' => $check['user_id'])
+				));
+			return $exists > 0;
+		}
+	}
 
 	/**
 	 * Generate a token.
@@ -61,6 +144,11 @@ class AuthenticationToken extends AppModel {
 			'user_id' => $userId,
 			'token' => self::generateToken(),
 		);
+		$this->set($token);
+		$v = $this->validates();
+		if (!$v) {
+			return false;
+		}
 		$this->create();
 		$s = $this->save($token);
 		return $s;
