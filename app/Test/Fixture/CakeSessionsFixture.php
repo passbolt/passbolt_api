b2c02505bb8e900d8b505929892d18a6097470c4
Kevin <kevin@enova-tech.net>
2015-05-07 20:36:07 +0530

    fixture for cakeSessions

---
+++ app/Test/Fixture/CakeSessionsFixture.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * CakeSessionsFixture
+ *
+ */
+class CakeSessionsFixture extends CakeTestFixture {
+
+	/**
+	 * Fields
+	 *
+	 * @var array
+	 */
+	public $fields = array(
+		'id' => array('type' => 'string', 'null' => false, 'key' => 'primary'),
+		'data' => array('type' => 'text', 'null' => true, 'default' => null),
+		'expires' => array('type' => 'integer', 'null' => true, 'default' => null),
+		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1))
+	);
+
+	/**
+	 * Records
+	 *
+	 * @var array
+	 */
+	public $records = array(
+	);
+
+}
