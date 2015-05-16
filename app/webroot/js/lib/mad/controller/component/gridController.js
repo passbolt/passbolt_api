e30265575fa62fe2310bd16ab57138c51c8fa5aa
Kevin <kevin@enova-tech.net>
2015-05-16 19:08:45 +0530

    #PASSBOLT-942 fixed grid. span has become div for the wrapper. This was the origin of the bug.

--- app/webroot/js/lib/mad/controller/component/gridController.js
+++ app/webroot/js/lib/mad/controller/component/gridController.js
@@ -194,7 +194,7 @@ steal(
 
 				if(columnModel.cellAdapter) {
 					var itemId = mappedItem.id;
-					var $cell = $('#' + itemId + ' .js_grid_column_' + columnModel.name + ' span');
+					var $cell = $('#' + itemId + ' .js_grid_column_' + columnModel.name + ' div');
 					var cellValue = mappedItem[columnModel.name];
 					columnModel.cellAdapter($cell, cellValue, mappedItem, item, columnModel);
 				}
@@ -207,7 +207,7 @@ steal(
 					// Ok it is costing : + z*n (z #columWidget; n #items) with this 
 					// part to insert the items and render widget if there is
 					var itemId = mappedItem[i].id;
-					var $cell = $('#' + itemId + ' .js_grid_column_' + columnModel.name + ' span');
+					var $cell = $('#' + itemId + ' .js_grid_column_' + columnModel.name + ' div');
 					widgetOptions.value = mappedItem[i][columnModel.name];
 					$cell[widgetJQueryPlugin](widgetOptions);
 					$cell[widgetJQueryPlugin]('render');
