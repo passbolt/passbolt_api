2bdf94570ece79428f2cd0ab4fc59409398969d5
Kevin <kevin@enova-tech.net>
2015-05-24 18:21:47 +0530

    #PASSBOLT-968 fixed breadcrumb for settings workspace

--- app/webroot/js/app/controller/component/settingsBreadcrumbController.js
+++ app/webroot/js/app/controller/component/settingsBreadcrumbController.js
@@ -40,25 +40,26 @@ steal(
 		'afterStart': function () {
 			// Create and render menu in the created container.
 			var menuSelector = '#' + this.getId() + ' ul';
-			this.options.menu = new mad.controller.component.MenuController(menuSelector, {
-				'itemTemplateUri': 'app/view/template/component/breadcrumb/breadcrumbItem.ejs'
-			});
+			this.options.menu = new mad.controller.component.MenuController(
+				menuSelector,
+				{
+					'itemTemplateUri': 'app/view/template/component/breadcrumb/breadcrumbItem.ejs'
+				}
+			);
 			this.options.menu.start();
-		},
 
-		/**
-		 * Parse the current filter
-		 * @param {passbolt.model.Filter} filter The filter to load
-		 * @return {array}
-		 */
-		'parseFilter': function (filter) {
-			var menuItems = [];
+			// Store menu items in an array.
+			// This contains the static part of the menu.
+			this.menuItems = [];
+			// Contains the specific section menu items.
+			this.sectionMenuItems = [];
 
-			// Add a link to filter on all items as first item.
+			// First 2 items of the menu are constant.
 			var menuItem = new mad.model.Action({
 				'id': uuid(),
 				'label': __('All users'),
 				'action': function () {
+					// Add a link to filter on all items as first item.
 					var filter = new passbolt.model.Filter({
 						'label': __('All users'),
 						'type': passbolt.model.Filter.SHORTCUT
@@ -69,38 +70,88 @@ steal(
 					mad.bus.trigger('filter_users_browser', filter);
 				}
 			});
-			menuItems.push(menuItem);
+			this.menuItems.push(menuItem);
 
 			var menuItem = new mad.model.Action({
 				'id': uuid(),
 				'label': passbolt.model.User.getCurrent().Profile.first_name + ' ' + passbolt.model.User.getCurrent().Profile.last_name,
 				'action': function () {
-					return;
+					// Switch to people workspace.
+					mad.bus.trigger('request_settings_section', 'profile');
 				}
 			});
-			menuItems.push(menuItem);
+			this.menuItems.push(menuItem);
 
-			var menuItem = new mad.model.Action({
-				'id': uuid(),
-				'label': __('Profile'),
-				'action': function () {
-					return;
-				}
-			});
-			menuItems.push(menuItem);
-
-			return menuItems;
+			// Specific menu items, per section.
+			// profile section.
+			this.sectionMenuItems['profile'] = [
+				new mad.model.Action({
+					'id': uuid(),
+					'label': __('Profile'),
+					'action': function () {
+						return;
+					}
+				})
+			];
+			// keys section.
+			this.sectionMenuItems['keys'] = [
+				new mad.model.Action({
+					'id': uuid(),
+					'label': __('Keys management'),
+					'action': function () {
+						return;
+					}
+				})
+			];
 		},
 
 		/**
 		 * Load the current filter
-		 * @param {passbolt.model.Filter} filter The filter to load
 		 */
-		'load': function (filter) {
-			var menuItems = this.parseFilter(filter);
+		'load': function () {
+			// To use if we need to load something.
+			// Do not remove, it breaks the code.
+		},
 
+		/**
+		 * Destroy the workspace.
+		 */
+		'destroy': function() {
+			// Be sure that the primary workspace menu controller will be destroyed also.
+			$('#' + this.getId() + ' ul').empty();
+			this._super();
+		},
+
+		/**
+		 * Refresh the menu items as per the section.
+		 * @param section
+		 */
+		'refreshMenuItems': function(section) {
+			// The items of the menu are a combination of static items and section dynamic items.
+			// If the section is recognised, we just assemble the 2 arrays. Otherwise, we just keep the static part.
+			var menuItems = (this.sectionMenuItems[section] !== undefined) ?
+				$.merge($.merge([], this.menuItems), this.sectionMenuItems[section]) : this.menuItems;
+			// Reset the menu.
 			this.options.menu.reset();
+			// Load the items.
 			this.options.menu.load(menuItems);
+		},
+
+		/* ************************************************************** */
+		/* LISTEN TO THE APP EVENTS */
+		/* ************************************************************** */
+
+		/**
+		 * Listen to request_settings_section event.
+		 * @param el
+		 * @param ev
+		 * @param section
+		 */
+		'{mad.bus} request_settings_section': function (el, ev, section) {
+			// @todo #PASSBOLT-985 fixed in future canJs.
+			if (!this.element) return;
+			// When the section changes, we refresh the menu items in the breadcrumb.
+			this.refreshMenuItems(section);
 		}
 
 	});
