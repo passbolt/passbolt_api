541bacb6d4df13f9fb6ee2ce34d627da6c708394
Kevin Muller <mynameisgnu@gmail.com>
2015-09-10 17:20:32 +0530

    #PASSBOLT-1027 moved files into app_old directory

---
+++ app/webroot/js/app/controller/comments.js
@@ -0,0 +1,101 @@
+import 'app/model/comment';
+import 'app/view/component/comments';
+import 'app/controller/component/comments_list';
+import 'app/controller/form/comment/create';
+
+/**
+ * @inherits mad.controller.component.ComponentController
+ * @parent index
+ *
+ * @constructor
+ * Creates a new Comments controller
+ *
+ * @param {HTMLElement} element the element this instance operates on.
+ * @param {Object} [options] option values for the controller.  These get added to
+ * this.options and merged with defaults static variable
+ * 	 - foreignModel : the model the comment system will be plugged to
+ * 	 - foreign Id : the resource id (foreign key) the comment system will be plugged to
+ * @return {passbolt.controller.CommentsController}
+ */
+var Comments = passbolt.component.Comments = mad.Component.extend('passbolt.component.Comments', /** @static */ {
+	defaults: {
+		label					: 'Comments Controller',
+		viewClass				: passbolt.view.component.Comments,
+		// the resource to bind the component on
+		resource				: this.options.resource,
+		foreignModel 			: null,
+		foreignId 				: null,
+		commentsListController	: null
+	}
+
+}, /** @prototype */ {
+
+	/**
+	 * Called right after the start function
+	 * @return {void}
+	 * @see {mad.controller.ComponentController}
+	 */
+	afterStart: function () {
+		// create a form to add a comment and plug it onto the current resource
+		this.addForm = new passbolt.form.comment.Create($('#js_rs_details_comments_add_form', this.element), {
+			'templateBased'	: true,
+			'templateUri'	: 'app/view/template/form/comment/addForm.ejs',
+			'foreignModel'	: this.options.foreignModel,
+			'foreignId'		: this.options.foreignId
+		});
+		//this.addForm.setViewData({'resource' : this.options.resource}); // This doesn't work. why ?
+		this.addForm.start();
+		// Hide the comment add form by default
+		this.addForm.setState('hidden');
+
+		// Instantiate the comments List controller
+		// It will take care of listing the comments
+		this.commentsList = new passbolt.controller.component.commentsList($('#js_rs_details_comments_list', this.element), {
+			'resource'		: this.options.resource,
+			'foreignModel'	: this.options.foreignModel,
+			'foreignId'		: this.options.foreignId
+		});
+		this.commentsList.start();
+	},
+
+	'{passbolt.model.Comment} created': function (model, ev, resource) {
+		var self = this;
+		// If the new resource belongs to one of the categories displayed by the resource
+		if (resource.foreign_id == this.options.resource.id) {
+			self.commentsList.insertItem(resource, null, 'first');
+			self.addForm.setState('hidden');
+			return false; // break
+		}
+	},
+
+	/**
+	 * Catches event request_delete_comment, and proceed with deleting a comment
+	 * @param model
+	 * @param ev
+	 * @param resource
+	 */
+	'{mad.bus} request_delete_comment' : function (model, ev, resource) {
+		// @todo fixed in future canJs.
+		if (!this.element) return;
+
+		resource.destroy(function(){
+			mad.bus.trigger('comment_deleted', resource);
+		});
+	},
+
+	/**
+	 * catches a comment_deleted event. (when a comment is successfully deleted)
+	 * @param model
+	 * @param ev
+	 * @param resource
+	 */
+	'{mad.bus} comment_deleted' : function (model, ev, resource) {
+		// @todo fixed in future canJs.
+		if (!this.element) return;
+
+		// Todo : user feedback
+		// Todo : nice animation on remove
+		this.commentsList.removeItem(resource);
+	}
+
+});
