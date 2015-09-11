d23e24d3ddcdb2de502b782fa3fdc7faaaccc880
Kevin Muller <mynameisgnu@gmail.com>
2015-09-11 18:02:26 +0530

    #PASSBOLT-1027 more components

---
+++ app/webroot/js/app/error/error_handler.js
@@ -0,0 +1,14 @@
+import "mad/error/error_handler";
+
+/**
+ * @inherits jQuery.Class
+ * @parent mad.core
+ *
+ * Common error handler.
+ * Use the console as logger.
+ */
+var ErrorHandler = passbolt.error.ErrorHandler = mad.error.ErrorHandler.extend('mad.error.ErrorHandler', /** @static */ {
+
+});
+
+export default ErrorHandler;
