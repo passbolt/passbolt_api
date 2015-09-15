109570c031d20a08f75161ce161364c5fc7e66ae
Kevin Muller <mynameisgnu@gmail.com>
2015-09-15 15:23:05 +0530

    #PASSBOLT-1027 fixed config

--- app/webroot/js/app/passbolt.js
+++ app/webroot/js/app/passbolt.js
@@ -10,11 +10,11 @@ import 'mad/mad';
 import 'mad/bootstrap';
 import passbolt from 'app/util/util'; // @todo rename to setting maybe.
 import 'app/error/error_handler';
+import 'app/net/response_handler';
 import 'app/component/app';
 import appConfig from 'app/config/config.json';
 
 $(document).ready(function () {
-
 	// Load the config packaged with the front-end application.
 	mad.Config.load(appConfig);
 
