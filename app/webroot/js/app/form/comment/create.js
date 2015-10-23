a24769376328a650e828755a95136bd0dfbd5b2f
Kevin Muller <mynameisgnu@gmail.com>
2015-10-23 11:37:14 +0530

    #PASSBOLT-1070 reinitialize the form each time it's hidden to avoid inline validation

--- app/webroot/js/app/form/comment/create.js
+++ app/webroot/js/app/form/comment/create.js
@@ -108,7 +108,18 @@ var Create = passbolt.form.comment.Create = mad.Form.extend('passbolt.form.comme
 	 */
 	'stateReady': function() {
 		this.options.commentContentField.setValue('');
-	}
+	},
+
+    /**
+     * State hidden.
+     * @param go
+     */
+    stateHidden: function (go) {
+        this._super(go);
+        // Reinitialize number of validations to avoid inline validation
+        // each time the form appears.
+        this.validations = 0;
+    }
 });
 
 export default Create;
