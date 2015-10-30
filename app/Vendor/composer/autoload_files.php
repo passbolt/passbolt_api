9f407276860ea7877feab40175f310430425d3da
Kevin Muller <mynameisgnu@gmail.com>
2015-10-30 20:07:01 +0530

    #PASSBOLT-925 added openpgp-php in composer

--- app/Vendor/composer/autoload_files.php
+++ app/Vendor/composer/autoload_files.php
@@ -6,5 +6,6 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    $vendorDir . '/phpseclib/phpseclib/phpseclib/Crypt/Random.php',
     $vendorDir . '/ezyang/htmlpurifier/library/HTMLPurifier.composer.php',
 );
