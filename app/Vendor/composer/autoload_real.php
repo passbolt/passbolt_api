601b1691765ddaaab424375134e0129cba27537b
Kevin Muller <mynameisgnu@gmail.com>
2015-10-30 20:04:47 +0530

    #PASSBOLT-925 added openpgp-php in composer

--- app/Vendor/composer/autoload_real.php
+++ app/Vendor/composer/autoload_real.php
@@ -23,6 +23,10 @@ class ComposerAutoloaderInit8a4a8532ccdd40cff3f7bcc19447dd98
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit8a4a8532ccdd40cff3f7bcc19447dd98', 'loadClassLoader'));
 
+        $includePaths = require __DIR__ . '/include_paths.php';
+        array_push($includePaths, get_include_path());
+        set_include_path(join(PATH_SEPARATOR, $includePaths));
+
         $map = require __DIR__ . '/autoload_namespaces.php';
         foreach ($map as $namespace => $path) {
             $loader->set($namespace, $path);
