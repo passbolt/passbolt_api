fce2c2f725c8c9885ad3b5451c2a127fde9e6563
Cedric Alfonsi <cedricalfonsi@gmail.com>
2016-03-07 10:51:21 +0100

    PASSBOLT-1343 Change the way the class detect if dependencies are loaded. Submit pull request to the project https://github.com/ThaDafinser/UserAgentParser/pull/60

--- app/Vendor/thadafinser/user-agent-parser/src/Provider/DonatjUAParser.php
+++ app/Vendor/thadafinser/user-agent-parser/src/Provider/DonatjUAParser.php
@@ -62,7 +62,7 @@ class DonatjUAParser extends AbstractProvider
 
     public function __construct()
     {
-        if (! file_exists('vendor/' . $this->getPackageName() . '/composer.json')) {
+        if (! function_exists('parse_user_agent')) {
             throw new PackageNotLoadedException('You need to install the package ' . $this->getPackageName() . ' to use this provider');
         }
     }
