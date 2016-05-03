832518590946414ec548702f450ec7e12cf4d2da
Kevin Muller <kevin.muller@enova-tech.net>
2016-05-03 12:08:27 +0530

    PASSBOLT-1526 version number 1.0.10

--- app/Config/version.php
+++ app/Config/version.php
@@ -1,7 +1,7 @@
 <?php
 $config = [
 	'App.version' => [
-		'number' => '1.0.9',
-		'name' => 'Let\'s stay together',
+		'number' => '1.0.10',
+		'name' => 'Sittin\' on the dock of the bay',
 	],
 ];
