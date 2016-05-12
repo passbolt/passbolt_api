dc7add69247a6787564693d7b2ce8d63cf0fb53f
remy <remy@stripthis.com>
2016-05-12 15:39:23 +0530

    Profile avatar swag

--- app/Model/ProfileAvatar.php
+++ app/Model/ProfileAvatar.php
@@ -7,13 +7,88 @@
  */
 App::uses('ImageStorage', 'FileStorage.Model');
 
+/**
+ * @SWG\Definition(
+ * @SWG\Xml(name="Profile"),
+ * @SWG\Property(
+ *     property="id",
+ *     type="string",
+ *     description="Profile UUID",
+ *     example="d1acbfc1-78d8-3e11-ad8b-7ab1eb0332d3"
+ *   ),
+ * @SWG\Property(
+ *     property="user_id",
+ *     type="string",
+ *     description="User UUID",
+ *     example="d1acbfc1-78d8-3e11-ad8b-7ab1eb0332d3"
+ *   ),
+ * @SWG\Property(
+ *     property="foreign_key",
+ *     type="string",
+ *     description="Related model UUID",
+ *     example="d1acbfc1-78d8-3e11-ad8b-7ab1eb0332d3"
+ *   ),
+ * @SWG\Property(
+ *     property="model",
+ *     type="string",
+ *     description="Name of the model the image is attached to",
+ *     example="﻿ProfileAvatar"
+ *   ),
+ * @SWG\Property(
+ *     property="filename",
+ *     type="string",
+ *     description="Original name of the file"
+ *   ),
+ * @SWG\Property(
+ *     property="filesize",
+ *     type="integer",
+ *     description="Size of the file in bytes",
+ *   ),
+ * @SWG\Property(
+ *     property="mime_type",
+ *     type="string",
+ *     description="Mime type",
+ *     example="﻿image/png"
+ *   ),
+ * @SWG\Property(
+ *     property="extension",
+ *     type="string",
+ *     description="File extension",
+ *     example="jpeg"
+ *   ),
+ * @SWG\Property(
+ *     property="adapter",
+ *     type="string",
+ *     description="Gauffrette storage adapter class",
+ *     example="Local"
+ *   ),
+ * @SWG\Property(
+ *     property="hash",
+ *     type="string",
+ *     description="Hash of the file"
+ *   ),
+ * @SWG\Property(
+ *     property="path",
+ *     type="string",
+ *     description="Path to the file from webroot",
+ *     example="﻿images/ProfileAvatar/56/37/42/571f51765cbc4eeabaf60b26f72375ee/"
+ *   ),
+ * @SWG\Property(
+ *     property="created",
+ *     type="string",
+ *     description="Creation date",
+ *     example="﻿2016-04-26 17:01:01"
+ *   ),
+ * @SWG\Property(
+ *     property="modified",
+ *     type="string",
+ *     description="Last modification date",
+ *     example="﻿2016-04-26 17:01:01"
+ *   )
+ * )
+ */
 class ProfileAvatar extends ImageStorage {
 
-/**
- * Defines which key to use while returning an avatar object
- * to tell the path for the avatar image.
- * @var string
- */
 /**
  * Defines which key to use while returning an avatar object
  * to tell the path for the avatar image.
@@ -35,7 +110,7 @@ class ProfileAvatar extends ImageStorage {
  *
  * @link http://api20.cakephp.org/class/model#
  */
-	public $_actsAs = [
+	protected $_actsAs = [
 		'FileStorage.UploadValidator' => [
 			'validate' => true,
 			'localFile' => true,
@@ -48,8 +123,10 @@ class ProfileAvatar extends ImageStorage {
 /**
  * beforeSave callback
  *
- * @param array $options
- * @return boolean true on success
+ * @param array $options Options passed from Model::save().
+ * @return bool True if the operation should continue, false if it should abort
+ * @link http://book.cakephp.org/2.0/en/models/callback-methods.html#beforesave
+ * @see Model::save()
  */
 	public function beforeSave($options = []) {
 		if (!parent::beforeSave($options)) {
@@ -66,13 +143,12 @@ class ProfileAvatar extends ImageStorage {
 
 /**
  * After find callback.
- *
  * Is used to build an array of url for the images.
  *
- * @param mixed $results
- * @param bool $primary
- *
- * @return mixed
+ * @param mixed $results The results of the find operation
+ * @param bool $primary Whether this model is being queried directly (vs. being queried as an association)
+ * @return mixed Result of the find operation
+ * @link http://book.cakephp.org/2.0/en/models/callback-methods.html#afterfind
  */
 	public function afterFind($results, $primary = false) {
 		if (isset($results[0])) {
@@ -88,8 +164,10 @@ class ProfileAvatar extends ImageStorage {
 /**
  * Upload a file
  *
- * @param $foreignId
- * @param $data
+ * @param string $foreignId uuid of the foreign model
+ * @param array $data data with Avatar key set
+ * @throws ValidationException if validation fails
+ * @throws Exception if save operation fails
  * @return mixed
  */
 	public function upload($foreignId, $data) {
@@ -140,7 +218,6 @@ class ProfileAvatar extends ImageStorage {
 			$this->delete($avatar['Avatar']['id']);
 		}
 
-
 		$this->create();
 		$save = $this->save($data);
 		if (!$save) {
@@ -155,7 +232,8 @@ class ProfileAvatar extends ImageStorage {
  *
  * @param array $image entry of the db
  * @param string $version version as defined in file storage configuration file.
- * @param array $options
+ * @param array $options options like 'version' to define size
+ * @throws InvalidArgumentException
  * @return bool
  */
 	public function imageUrl($image, $version = null, $options = array()) {
@@ -178,8 +256,11 @@ class ProfileAvatar extends ImageStorage {
 		if (!empty($version)) {
 			$hash = Configure::read('Media.imageHashes.' . $image['model'] . '.' . $version);
 			if (empty($hash)) {
+				if (empty($image['model']) || !isset($image['model'])) {
+					$image['model'] = 'undefined';
+				}
 				throw new \InvalidArgumentException(__d('file_storage', 'No valid version key (%s %s) passed!',
-					@$image['model'], $version));
+					$image['model'], $version));
 			}
 		} else {
 			$hash = null;
@@ -202,15 +283,15 @@ class ProfileAvatar extends ImageStorage {
 	}
 
 /**
- * Turns the windows \ into / so that the path can be used in an url
+ * Turns the windows DS into linux one
+ * E.g. \ into / so that the path can be used in an url
  *
- * @param string $path
- * @return string
+ * @param string $path a file path like\this\one
+ * @return string a file path like/this/one
  */
 	public function normalizePath($path) {
 		return str_replace('\\', '/', $path);
 	}
-
 
 /**
  * Add avatar path information for each available size to the model.
