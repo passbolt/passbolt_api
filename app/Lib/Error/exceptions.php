37b326d5168b17f9969a0fe0d1996147f9c21d2a
remy <remy@stripthis.com>
2016-05-19 16:40:09 +0530

    PASSBOLT-1559 PHPCS for lib

--- app/Lib/Error/exceptions.php
+++ app/Lib/Error/exceptions.php
@@ -6,13 +6,25 @@
  * @licence GNU Affero General Public License http://www.gnu.org/licenses/agpl-3.0.en.html
  */
 class ValidationException extends Exception {
+
 	public $invalidFields = null;
 
+/**
+ * Construct the exception.
+ *
+ * @param string $message the Exception message to throw.
+ * @param array $invalidFields fields missing validation and associated error messages
+ */
 	public function __construct($message, array $invalidFields) {
 		parent::__construct($message);
 		$this->invalidFields = $invalidFields;
 	}
 
+/**
+ * Getter for validation error details
+ *
+ * @return array|null  fields missing validation and associated error messages
+ */
 	public function getInvalidFields() {
 		return $this->invalidFields;
 	}
