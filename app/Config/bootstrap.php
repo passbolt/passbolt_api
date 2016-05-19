ae778bece769739fc61f813303a4ca8b5b996bec
remy <remy@stripthis.com>
2016-05-19 17:25:40 +0530

    PASSBOLT-1559 fixing boostrap regression

--- app/Config/bootstrap.php
+++ app/Config/bootstrap.php
@@ -41,16 +41,8 @@ Purifier::config('nohtml', array(
 ));
 
 /**
- * Imagine plugin for the image manipulation.
- */
-CakePlugin::load('Imagine', array(
-	'bootstrap' => true
-));
-
-/**
  * File storage plugin for file manipulation.
  */
-require_once (APP . 'Config' . DS . 'file_storage.php');
 CakePlugin::load('FileStorage', array(
 	'bootstrap' => true
 ));
@@ -58,23 +50,12 @@ CakePlugin::load('Imagine', array(
 	'bootstrap' => true
 ));
 CakePlugin::load('EmailQueue');
+require_once (APP . 'Config' . DS . 'file_storage.php');
 
 /**
- * You can attach event listeners to the request lifecycle as Dispatcher Filter. By default CakePHP bundles two filters:
- *
- * - AssetDispatcher filter will serve your asset files (css, images, js, etc) from your themes and plugins
- * - CacheDispatcher filter will read the Cache.check configure variable and try to serve cached content generated from controllers
- *
- * Feel free to remove or add filters as you see fit for your application. A few examples:
- *
- * Configure::write('Dispatcher.filters', array(
- *		'MyCacheFilter', //  will use MyCacheFilter class from the Routing/Filter package in your app.
- *		'MyCacheFilter' => array('prefix' => 'my_cache_'), //  will use MyCacheFilter class from the Routing/Filter package in your app with settings array.
- *		'MyPlugin.MyFilter', // will use MyFilter class from the Routing/Filter package in MyPlugin plugin.
- *		array('callable' => $aFunction, 'on' => 'before', 'priority' => 9), // A valid PHP callback type to be called on beforeDispatch
- *		array('callable' => $anotherMethod, 'on' => 'after'), // A valid PHP callback type to be called on afterDispatch
- *
- * ));
+ * Attach event listeners to the request lifecycle as Dispatcher Filter.
+ * AssetDispatcher serve asset files (css, images, js, etc) from your themes and plugins
+ * CacheDispatcher read the Cache.check configure variable and try to serve cached content generated from controllers
  */
 Configure::write('Dispatcher.filters', array(
 	'AssetDispatcher',
