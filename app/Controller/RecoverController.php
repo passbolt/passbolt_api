66597c5d7b40bf4b95f926e93bc139cb33511641
remy <remy@passbolt.com>
2016-12-28 10:46:25 +0530

    PASSBOLT-1619 Recover should not work for accounts with incomplete setup

---
+++ app/Controller/RecoverController.php
@@ -0,0 +1,121 @@
+<?php
+/**
+ * Account Recovery Controller
+ *
+ * @copyright (c) 2016-present Bolt Softwares Pvt Ltd
+ * @licence GNU Affero General Public License http://www.gnu.org/licenses/agpl-3.0.en.html
+ */
+App::uses('Core', 'Validation');
+
+class RecoverController extends AppController {
+
+/**
+ * Controller name
+ *
+ * @var string
+ */
+    public $name = 'Pages';
+
+/**
+ * This controller uses the User model
+ *
+ * @var array
+ */
+    public $uses = ['User','AuthenticationToken'];
+
+/**
+ * @var array components used by this controller
+ */
+    public $components = [
+        'EmailNotificator',
+    ];
+
+/**
+ * Called before the controller action. Used to manage access right
+ *
+ * @return void
+ * @link http://book.cakephp.org/2.0/en/controllers.html#request-life-cycle-callbacks
+ */
+    public function beforeFilter() {
+        $allow = [
+            'recover',
+            'recover_thankyou',
+        ];
+        $this->Auth->allow($allow);
+        parent::beforeFilter();
+    }
+
+/**
+ * Recover an existing account page.
+ *
+ * @throws Exception
+ * @return void
+ */
+    public function recover() {
+        $this->layout = 'login';
+
+        if (!empty($this->request->data)) {
+            try {
+                // No user or invalid data provided
+                if (!isset($this->request->data['User']) || empty($this->request->data['User']) ||
+                    !isset($this->request->data['User']['username']) || empty($this->request->data['User']['username'])) {
+                    throw new BadRequestException(__('Please provide an email address.'));
+                }
+                if (!Validation::email($this->request->data['User']['username'])) {
+                    throw new BadRequestException(__('Please provide a valid email address.'));
+                }
+
+                // User doesn't exist.
+                $user = $this->User->findByUsername($this->request->data['User']['username']);
+                if (empty($user)) {
+                    throw new BadRequestException(__('This user does not exist. Please register and complete the setup first.'));
+                }
+
+                // User has been deleted
+                if ($user['User']['deleted']) {
+                    throw new BadRequestException(__('This user has been deleted. Please contact your administrator.'));
+                }
+
+                // User is not activated
+                if (!$user['User']['active']) {
+                    throw new BadRequestException(__('This user is not active. Please complete the setup first.'));
+                }
+
+                // Create the setup authentication token
+                $saveToken = $this->AuthenticationToken->generate($user['User']['id']);
+                if (!$saveToken) {
+                    throw new InternalErrorException(__('Something went wrong and it was not possible to create a recovery token. Please try again later.'));
+                }
+            }
+            catch (Exception $e) {
+                if ($this->request->is('json')) {
+                    throw $e;
+                }
+
+                $this->User->invalidate('username', $e->getMessage());
+                return;
+            }
+
+            // Send notification email
+            $this->EmailNotificator->accountRecoveryNotification(
+                $user['User']['id'],
+                [
+                    'token' => $saveToken,
+                    'creator_id' => $user['User']['id'],
+                ]);
+
+            $this->redirect('/recover/thankyou');
+        }
+    }
+
+/**
+ * Thank you page after recovery.
+ *
+ * @throws Exception
+ * @return void
+ */
+    public function recover_thankyou() {
+        $this->layout = 'login';
+    }
+
+}
---
+++ app/Test/Case/Controller/RecoverControllerTest.php
@@ -0,0 +1,156 @@
+<?php
+/**
+ * Users Controller Tests
+ *
+ * @copyright (c) 2015-present Bolt Softwares Pvt Ltd
+ * @licence GNU Affero General Public License http://www.gnu.org/licenses/agpl-3.0.en.html
+ */
+App::uses('AppController', 'Controller');
+App::uses('UsersController', 'Controller');
+App::uses('User', 'Model');
+App::uses('Role', 'Model');
+
+class RecoverControllerTest extends ControllerTestCase
+{
+
+    public $fixtures = array(
+        'app.user',
+        'app.role',
+        'app.email_queue',
+        'app.user_agent',
+        'app.profile',
+        'app.file_storage',
+        'app.authenticationToken',
+        'app.authenticationLog',
+        'app.controller_log',
+    );
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->User = Common::getModel('User');
+        $this->Gpgkey = Common::getModel('Gpgkey');
+    }
+
+    public function tearDown()
+    {
+        parent::tearDown();
+    }
+
+/**
+ * Test recovery with no username
+ *
+ * @return void
+ */
+    public function testRecoverNoUsername() {
+        $data = [
+            'User' => [
+                'username' => '',
+            ]
+        ];
+        $this->setExpectedException('BadRequestException', 'Please provide an email address.');
+        $this->testAction('/recover.json', ['return' => 'view', 'method' => 'post', 'data' => $data], true);
+    }
+
+/**
+ * Test recovery with invalid email
+ *
+ * @return void
+ */
+    public function testRecoverInvalidUsername() {
+        $data = [
+            'User' => [
+                'username' => 'test@test',
+            ]
+        ];
+        $this->setExpectedException('BadRequestException', 'Please provide a valid email address.');
+        $this->testAction('/recover.json', ['return' => 'view', 'method' => 'post', 'data' => $data], true);
+    }
+
+/**
+ * Test recovery with invalid email (NOT JSON)
+ *
+ * @return void
+ */
+    public function testRecoverInvalidUsernameNotJSON() {
+        $data = [
+            'User' => [
+                'username' => 'test@test',
+            ]
+        ];
+        $view = $this->testAction('/recover', ['return' => 'view', 'method' => 'post', 'data' => $data], true);
+        $this->assertContains('Please provide a valid email address.', $view);
+    }
+
+/**
+ * Test recovery with non existing user
+ *
+ * @return void
+ */
+    public function testRecoverUserDoesNotExist() {
+        $data = [
+            'User' => [
+                'username' => 'not_an_existing_email@passbolt.com',
+            ]
+        ];
+        $this->setExpectedException('BadRequestException', 'This user does not exist. Please register and complete the setup first.');
+        $this->testAction('/recover.json', ['return' => 'view', 'method' => 'post', 'data' => $data], true);
+    }
+
+/**
+ * Test recovery with deleted user
+ *
+ * @return void
+ */
+    public function testRecoverNonActiveUser() {
+        $data = [
+            'User' => [
+                'username' => 'orna@passbolt.com',
+            ]
+        ];
+        $this->setExpectedException('BadRequestException', 'This user is not active. Please complete the setup first.');
+        $this->testAction('/recover.json', ['return' => 'view', 'method' => 'post', 'data' => $data], true);
+    }
+
+/**
+ * Test recovery with non active user
+ *
+ * @return void
+ */
+    public function testRecoverDeletedUser() {
+        $data = [
+            'User' => [
+                'username' => 'ruth@passbolt.com',
+            ]
+        ];
+        $this->setExpectedException('BadRequestException', 'This user has been deleted. Please contact your administrator.');
+        $this->testAction('/recover.json', ['return' => 'view', 'method' => 'post', 'data' => $data], true);
+    }
+
+/**
+ * Test recovery with non active user
+ *
+ * @return void
+ */
+    public function testRecoverValidUser() {
+        $data = [
+            'User' => [
+                'username' => 'ada@passbolt.com',
+            ]
+        ];
+        $result = $this->testAction('/recover', ['return' => 'view', 'method' => 'post', 'data' => $data], true);
+        // empty because of redirection
+        $this->assertEmpty($result);
+    }
+
+/**
+ * Test recovery with non active user
+ *
+ * @return void
+ */
+    public function testThankyou() {
+        $result = $this->testAction('/recover/thankyou', ['return' => 'view', 'method' => 'GET'], true);
+        $this->assertContains('Email sent', $result);
+    }
+
+}
