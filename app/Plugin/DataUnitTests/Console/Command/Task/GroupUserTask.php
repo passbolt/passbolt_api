9262705dc8fde51ca058495dbb7bccda2ac424fd
Kevin Muller <kevin@passbolt.com>
2017-03-04 16:36:47 +0100

    Merge branch 'feature/PASSBOLT-1838-groups' of bitbucket.org:passbolt/passbolt into feature/PASSBOLT-1938
    # Conflicts:
    #	app/Plugin/DataUnitTests/Console/Command/Task/GroupUserTask.php
    #	app/Test/Fixture/FileStorageFixture.php
    #	app/Test/Fixture/GpgkeyFixture.php
    #	app/Test/Fixture/GroupFixture.php
    #	app/Test/Fixture/GroupsUserFixture.php
    #	app/Test/Fixture/PermissionFixture.php
    #	app/Test/Fixture/ProfileFixture.php
    #	app/Test/Fixture/ResourceFixture.php
    #	app/Test/Fixture/SchemaMigrationFixture.php
    #	app/Test/Fixture/SecretFixture.php
    #	app/Test/Fixture/UserFixture.php

--- app/Plugin/DataUnitTests/Console/Command/Task/GroupUserTask.php
+++ app/Plugin/DataUnitTests/Console/Command/Task/GroupUserTask.php
@@ -23,6 +23,7 @@ class GroupUserTask extends ModelTask {
 			'id' => Common::uuid('group_user.id.accounting-frances'),
 			'group_id' => Common::uuid('group.id.accounting'),
 			'user_id' => Common::uuid('user.id.frances'),
+			'is_admin' => 1,
 			'created' => '2016-01-29 13:39:25',
 			'modified' => '2016-01-29 13:39:25',
 			'created_by' => Common::uuid('user.id.admin'),
@@ -32,6 +33,7 @@ class GroupUserTask extends ModelTask {
 			'id' => Common::uuid('group_user.id.accounting-grace'),
 			'group_id' => Common::uuid('group.id.accounting'),
 			'user_id' => Common::uuid('user.id.grace'),
+			'is_admin' => 0,
 			'created' => '2016-01-29 13:39:25',
 			'modified' => '2016-01-29 13:39:25',
 			'created_by' => Common::uuid('user.id.admin'),
@@ -41,6 +43,7 @@ class GroupUserTask extends ModelTask {
 			'id' => Common::uuid('group_user.id.board-hedy'),
 			'group_id' => Common::uuid('group.id.board'),
 			'user_id' => Common::uuid('user.id.hedy'),
+			'is_admin' => 1,
 			'created' => '2016-01-29 13:39:25',
 			'modified' => '2016-01-29 13:39:25',
 			'created_by' => Common::uuid('user.id.admin'),
@@ -50,6 +53,7 @@ class GroupUserTask extends ModelTask {
 			'id' => Common::uuid('group_user.id.creative-irene'),
 			'group_id' => Common::uuid('group.id.creative'),
 			'user_id' => Common::uuid('user.id.irene'),
+			'is_admin' => 1,
 			'created' => '2016-01-29 13:39:25',
 			'modified' => '2016-01-29 13:39:25',
 			'created_by' => Common::uuid('user.id.anonymous'),
@@ -59,6 +63,7 @@ class GroupUserTask extends ModelTask {
 			'id' => Common::uuid('group_user.id.developer-irene'),
 			'group_id' => Common::uuid('group.id.developer'),
 			'user_id' => Common::uuid('user.id.irene'),
+			'is_admin' => 1,
 			'created' => '2016-01-29 13:39:25',
 			'modified' => '2016-01-29 13:39:25',
 			'created_by' => Common::uuid('user.id.admin'),
@@ -68,6 +73,7 @@ class GroupUserTask extends ModelTask {
 			'id' => Common::uuid('group_user.id.ergonom-irene'),
 			'group_id' => Common::uuid('group.id.ergonom'),
 			'user_id' => Common::uuid('user.id.irene'),
+			'is_admin' => 1,
 			'created' => '2016-01-29 13:39:25',
 			'modified' => '2016-01-29 13:39:25',
 			'created_by' => Common::uuid('user.id.admin'),
@@ -77,6 +83,7 @@ class GroupUserTask extends ModelTask {
 			'id' => Common::uuid('group_user.id.freelancer-jean'),
 			'group_id' => Common::uuid('group.id.freelancer'),
 			'user_id' => Common::uuid('user.id.jean'),
+			'is_admin' => 1,
 			'created' => '2016-01-29 13:39:25',
 			'modified' => '2016-01-29 13:39:25',
 			'created_by' => Common::uuid('user.id.admin'),
@@ -86,6 +93,7 @@ class GroupUserTask extends ModelTask {
 			'id' => Common::uuid('group_user.id.freelancer-kathleen'),
 			'group_id' => Common::uuid('group.id.freelancer'),
 			'user_id' => Common::uuid('user.id.kathleen'),
+			'is_admin' => 1,
 			'created' => '2016-01-29 13:39:25',
 			'modified' => '2016-01-29 13:39:25',
 			'created_by' => Common::uuid('user.id.admin'),
@@ -95,6 +103,7 @@ class GroupUserTask extends ModelTask {
 			'id' => Common::uuid('group_user.id.freelancer-lynne'),
 			'group_id' => Common::uuid('group.id.freelancer'),
 			'user_id' => Common::uuid('user.id.lynne'),
+			'is_admin' => 1,
 			'created' => '2016-01-29 13:39:25',
 			'modified' => '2016-01-29 13:39:25',
 			'created_by' => Common::uuid('user.id.admin'),
@@ -104,6 +113,7 @@ class GroupUserTask extends ModelTask {
 			'id' => Common::uuid('group_user.id.freelancer-marlyn'),
 			'group_id' => Common::uuid('group.id.freelancer'),
 			'user_id' => Common::uuid('user.id.marlyn'),
+			'is_admin' => 0,
 			'created' => '2016-01-29 13:39:25',
 			'modified' => '2016-01-29 13:39:25',
 			'created_by' => Common::uuid('user.id.admin'),
@@ -113,11 +123,83 @@ class GroupUserTask extends ModelTask {
 			'id' => Common::uuid('group_user.id.freelancer-nancy'),
 			'group_id' => Common::uuid('group.id.freelancer'),
 			'user_id' => Common::uuid('user.id.nancy'),
+			'is_admin' => 0,
 			'created' => '2016-01-29 13:39:25',
 			'modified' => '2016-01-29 13:39:25',
 			'created_by' => Common::uuid('user.id.admin'),
 			'modified_by' => Common::uuid('user.id.admin')
 		));
+		$gu[] = array('GroupUser' => array(
+			'id' => Common::uuid('group_user.id.human_resource-nancy'),
+			'group_id' => Common::uuid('group.id.human_resource'),
+			'user_id' => Common::uuid('user.id.nancy'),
+			'is_admin' => 1,
+			'created' => '2016-01-29 13:39:25',
+			'modified' => '2016-01-29 13:39:25',
+			'created_by' => Common::uuid('user.id.admin'),
+			'modified_by' => Common::uuid('user.id.admin')
+		));
+		$gu[] = array('GroupUser' => array(
+			'id' => Common::uuid('group_user.id.management-ada'),
+			'group_id' => Common::uuid('group.id.management'),
+			'user_id' => Common::uuid('user.id.ada'),
+			'is_admin' => 1,
+			'created' => '2016-01-29 13:39:25',
+			'modified' => '2016-01-29 13:39:25',
+			'created_by' => Common::uuid('user.id.admin'),
+			'modified_by' => Common::uuid('user.id.admin')
+		));
+		$gu[] = array('GroupUser' => array(
+			'id' => Common::uuid('group_user.id.marketing-ada'),
+			'group_id' => Common::uuid('group.id.marketing'),
+			'user_id' => Common::uuid('user.id.ada'),
+			'is_admin' => 1,
+			'created' => '2016-01-29 13:39:25',
+			'modified' => '2016-01-29 13:39:25',
+			'created_by' => Common::uuid('user.id.admin'),
+			'modified_by' => Common::uuid('user.id.admin')
+		));
+		$gu[] = array('GroupUser' => array(
+			'id' => Common::uuid('group_user.id.network-frances'),
+			'group_id' => Common::uuid('group.id.network'),
+			'user_id' => Common::uuid('user.id.frances'),
+			'is_admin' => 1,
+			'created' => '2016-01-29 13:39:25',
+			'modified' => '2016-01-29 13:39:25',
+			'created_by' => Common::uuid('user.id.admin'),
+			'modified_by' => Common::uuid('user.id.admin')
+		));
+		$gu[] = array('GroupUser' => array(
+			'id' => Common::uuid('group_user.id.sales-ada'),
+			'group_id' => Common::uuid('group.id.sales'),
+			'user_id' => Common::uuid('user.id.ada'),
+			'is_admin' => 1,
+			'created' => '2016-01-29 13:39:25',
+			'modified' => '2016-01-29 13:39:25',
+			'created_by' => Common::uuid('user.id.admin'),
+			'modified_by' => Common::uuid('user.id.admin')
+		));
+		$gu[] = array('GroupUser' => array(
+			'id' => Common::uuid('group_user.id.traffic-ada'),
+			'group_id' => Common::uuid('group.id.traffic'),
+			'user_id' => Common::uuid('user.id.ada'),
+			'is_admin' => 1,
+			'created' => '2016-01-29 13:39:25',
+			'modified' => '2016-01-29 13:39:25',
+			'created_by' => Common::uuid('user.id.admin'),
+			'modified_by' => Common::uuid('user.id.admin')
+		));
+		$gu[] = array('GroupUser' => array(
+			'id' => Common::uuid('group_user.id.deleted-ada'),
+			'group_id' => Common::uuid('group.id.deleted'),
+			'user_id' => Common::uuid('user.id.ada'),
+			'is_admin' => 1,
+			'created' => '2016-01-29 13:39:25',
+			'modified' => '2016-01-29 13:39:25',
+			'created_by' => Common::uuid('user.id.admin'),
+			'modified_by' => Common::uuid('user.id.admin')
+		));
+
 		return $gu;
 	}
 }
