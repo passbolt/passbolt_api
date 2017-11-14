9b07543db8b8e53caf0c407a36c8d6d24c6cbabc
Cédric Alfonsi <cedric@passbolt.com>
2017-11-14 17:10:36 +0100

    PASSBOLT-2500 Resources delete controller

---
+++ src/Controller/Resources/ResourcesDeleteController.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * Passbolt ~ Open source password manager for teams
+ * Copyright (c) Passbolt SARL (https://www.passbolt.com)
+ *
+ * Licensed under GNU Affero General Public License version 3 of the or any later version.
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Passbolt SARL (https://www.passbolt.com)
+ * @license       https://opensource.org/licenses/AGPL-3.0 AGPL License
+ * @link          https://www.passbolt.com Passbolt(tm)
+ * @since         2.0.0
+ */
+
+namespace App\Controller\Resources;
+
+use App\Controller\AppController;
+use App\Error\Exception\ValidationRuleException;
+use Cake\Datasource\Exception\RecordNotFoundException;
+use Cake\Network\Exception\BadRequestException;
+use Cake\Network\Exception\ForbiddenException;
+use Cake\Network\Exception\NotFoundException;
+use Cake\Validation\Validation;
+
+class ResourcesDeleteController extends AppController
+{
+    /**
+     * Resource Delete action
+     *
+     * @param string $id The identifier of the resource to delete.
+     * @throws NotFoundException If the resource does not exist.
+     * @throws NotFoundException If the resource is soft deleted.
+     * @throws NotFoundException If the user does not have access to the resource.
+     * @throws ForbiddenException If the user does not have the permission to delete the resource.
+     * @throws BadRequestException If the resource id is not a valid uuid.
+     * @return void
+     */
+    public function delete($id)
+    {
+        // Check request sanity
+        if (!Validation::uuid($id)) {
+            throw new BadRequestException(__('The resource id is not valid.'));
+        }
+
+        $this->loadModel('Resources');
+
+        // Retrieve the resource to delete.
+        try {
+            $resource = $this->Resources->get($id);
+        }
+        catch(RecordNotFoundException $e) {
+            throw new NotFoundException(__('The resource does not exist.'));
+        }
+
+        // Save the entity
+        $this->Resources->softDelete($this->User->id(), $resource);
+        $this->_handleDeleteError($resource);
+
+        $this->success(__('The resource was deleted'));
+    }
+
+    /**
+     * Manage delete errors.
+     *
+     * @param \Cake\Datasource\EntityInterface $resource
+     * @throws NotFoundException
+     * @throws ValidationRuleException
+     */
+    protected function _handleDeleteError($resource)
+    {
+        $errors = $resource->getErrors();
+        if (empty($errors)) {
+            return;
+        }
+
+        if (isset($errors['deleted']['is_not_soft_deleted'])) {
+            throw new NotFoundException(__('The resource does not exist.'));
+        }
+        else if (isset($errors['id']['has_access'])) {
+            // If the user has a read access return a 403, otherwise return a 404 to avoid data leak.
+            if ($this->Resources->hasAccess($this->User->id(), $resource->id)) {
+                throw new ForbiddenException(__('You do not have the permission to delete this resource.'));
+            }
+            throw new NotFoundException(__('The resource does not exist.'));
+        }
+
+        throw new ValidationRuleException(__('Could not delete the resource.'), $errors, $this->Resources);
+    }
+}
---
+++ tests/TestCase/Controller/Resources/ResourcesDeleteControllerTest.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Passbolt ~ Open source password manager for teams
+ * Copyright (c) Passbolt SARL (https://www.passbolt.com)
+ *
+ * Licensed under GNU Affero General Public License version 3 of the or any later version.
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Passbolt SARL (https://www.passbolt.com)
+ * @license       https://opensource.org/licenses/AGPL-3.0 AGPL License
+ * @link          https://www.passbolt.com Passbolt(tm)
+ * @since         2.0.0
+ */
+
+namespace App\Test\TestCase\Controller\Resources;
+
+use App\Test\Lib\AppIntegrationTestCase;
+use App\Utility\Gpg;
+use App\Utility\UuidFactory;
+use Cake\ORM\TableRegistry;
+use Cake\Utility\Hash;
+
+class ResourcesDeleteControllerTest extends AppIntegrationTestCase
+{
+    public $fixtures = ['app.users', 'app.groups', 'app.favorites', 'app.groups_users', 'app.resources', 'app.permissions'];
+
+    public function setUp()
+    {
+        $this->Resources = TableRegistry::get('Resources');
+        $this->gpg = new Gpg();
+        parent::setUp();
+    }
+
+    public function testDeleteSuccess()
+    {
+        $this->authenticateAs('ada');
+        $resourceId = UuidFactory::uuid('resource.id.apache');
+        $this->deleteJson("/resources/$resourceId.json");
+        $this->assertSuccess();
+    }
+
+    public function testDeleteErrorResourceIsSoftDeleted()
+    {
+        $this->authenticateAs('ada');
+        $resourceId = UuidFactory::uuid('resource.id.jquery');
+        $this->deleteJson("/resources/$resourceId.json");
+        $this->assertError(404, 'The resource does not exist.');
+    }
+
+    public function testDeleteErrorAccessDenied()
+    {
+        $this->authenticateAs('ada');
+        $resourceId = UuidFactory::uuid('resource.id.april');
+        $this->deleteJson("/resources/$resourceId.json");
+        $this->assertError(404, 'The resource does not exist.');
+    }
+
+    public function testDeleteErrorAccessDenied_ReadAccess()
+    {
+        $this->authenticateAs('ada');
+        $resourceId = UuidFactory::uuid('resource.id.bower');
+        $this->deleteJson("/resources/$resourceId.json");
+        $this->assertError(403, 'You do not have the permission to delete this resource.');
+    }
+
+    public function testDeleteErrorNotAuthenticated()
+    {
+        $resourceId = UuidFactory::uuid('resource.id.apache');
+        $this->deleteJson("/resources/$resourceId.json");
+        $this->assertAuthenticationError();
+    }
+}
