2712a3817ac90fddda41780a77f83086677c83b1
Diego Lendoiro <diego@inestable.org>
2018-01-05 13:02:05 +0100

    database configuration as environment variables

--- config/app.default.php
+++ config/app.default.php
@@ -222,16 +222,17 @@ return [
             'className' => 'Cake\Database\Connection',
             'driver' => 'Cake\Database\Driver\Mysql',
             'persistent' => false,
-            'host' => 'localhost',
+            'host' => env('PASSBOLT_DB_HOST','localhost'),
+            'port' => env('PASSBOLT_DB_PORT', 3306),
             /**
              * CakePHP will use the default DB port based on the driver selected
              * MySQL on MAMP uses port 8889, MAMP users will want to uncomment
              * the following line and set the port accordingly
              */
             //'port' => 'non_standard_port_number',
-            'username' => 'my_app',
-            'password' => 'secret',
-            'database' => 'my_app',
+            'username' => env('PASSBOLT_DB_USER','my_app'),
+            'password' => env('PASSBOLT_DB_PASS','secret'),
+            'database' => env('PASSBOLT_DB_NAME','my_app'),
             'encoding' => 'utf8mb4',
             'timezone' => 'UTC',
             'flags' => [],
