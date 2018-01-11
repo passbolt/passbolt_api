af1de47ff523e833ef3033ed54110e92bad30a86
Remy Bertot <remy@passbolt.com>
2018-01-11 12:02:54 +0530

    Worsmithing

--- config/passbolt.default.php
+++ config/passbolt.default.php
@@ -12,95 +12,38 @@
  * @link          https://www.passbolt.com Passbolt(tm)
  * @since         2.0.0
  */
-
 /**
- * Passbolt configuration options.
+ * PASSBOLT CONFIGURATION FILE TEMPLATE
  *
- * To see all available options, you can refer to the default.php file, and modify this file accordingly.
+ * By default passbolt try to use the environment variables or fallback on the default values as
+ * defined in default.php. You can use passbolt.default.php as a basis to set your own configuration
+ * without using environment variables.
+ *
+ * 1. copy/paste passbolt.default.php to passbolt.php
+ * 2. set the variables in the App section
+ * 3. set the variables in the passbolt section
+ *
+ * To see all available options, you can refer to the default.php file, and modify passsbolt.php accordingly.
+ * Do not modify default.php or you may break your upgrade process.
  *
  * Read more about how to install passbolt: https://www.passbolt.com/help/tech/install
  * Any issue, check out our FAQ: https://www.passbolt.com/faq
- * A question? Ask the community: https://community.passbolt.com/
+ * An installation issue? Ask for help to the community: https://community.passbolt.com/
  */
 return [
 
-    ///////////////////////////////////////////////////////////////////////////////////////////
-    // DEFAULT CONFIGURATION
-    // This is the default configuration.
-    // It enforces the use of ssl, and doesn't provide a default gpg key.
-    // If your objective is to try passbolt quickly for demo purpose, you can use the demo config example provided below.
-    ///////////////////////////////////////////////////////////////////////////////////////////
-    'passbolt' => [
-        // Define if users can register by themselves.
-        'registration' => [
-            'public' => false,
-        ],
-        // Enforce the use of ssl (requires a https confguration and certificate on your web server).
-        'ssl' => [
-            'force' => true,
-        ],
-        // GPG Configuration.
-        'gpg' => [
-            // Tell GPG where to find the keyring.
-            // Needs to be available by the webserver user, for example:
-            // Apache on Centos it would be in '/usr/share/httpd/.gnupg'
-            // Apache on Debian it would be in '/var/www/.gnupg'
-            // Nginx on Centos it would be in '/var/lib/nginx/.gnupg'
-            // etc.
-            'keyring' => '/home/www-data/.gnupg',
-
-            // Replace GNUPGHOME with above value even if it is set.
-            'putenv' => true,
-
-            // Main server key.
-            'serverKey' => [
-                // Server private key fingerprint.
-                'fingerprint' => '',
-                // Server public / private key file location.
-                'public' => '',
-                'private' => '',
-            ],
-        ],
-    ],
-
-    ///////////////////////////////////////////////////////////////////////////////////////////
-    // DEMO CONFIGURATION EXAMPLE.
-    // Uncomment the lines below if you want to try passbolt quickly.
-    // and if you are not concerned about the security of your installation.
-    // (Don't forget to comment the default config above).
-    ///////////////////////////////////////////////////////////////////////////////////////////
-//    // Activate debug mode.
-//    'debug' => true,
-//    'passbolt' => [
-//        'registration' => [
-//            'public' => true
-//        ],
-//        'ssl' => [
-//            'force' => false,
-//        ],
-//        'gpg' => [
-//            // Tell GPG where to find the keyring
-//            // Needs to be available by the webserver user, for example:
-//            // Apache on Centos it would be in '/usr/share/httpd/.gnupg'
-//            // Apache on Debian it would be in '/home/www-data/.gnupg'
-//            // Nginx on Centos it would be in '/var/lib/nginx/.gnupg'
-//            // etc.
-//            'keyring' => '/var/lib/nginx/.gnupg',
-//            'putenv' => true,
-//            'serverKey' => [
-//                'fingerprint' => '2FC8945833C51946E937F9FED47B0811573EE67E',
-//                'public' => ROOT . DS . 'config' . DS . 'gpg' . DS . 'unsecure.key',
-//                'private' => ROOT . DS . 'config' . DS . 'gpg' . DS . 'unsecure_private.key',
-//            ],
-//        ],
-//    ],
-    //////////////////////////// END DEMO CONFIGURATION EXAMPLE ////////////////////////////
-
-    // App configuration.
+    /**
+     * DEFAULT PASSBOLT CONFIGURATION
+     *
+     * All the information in this section must be provided in order for passbolt to work
+     * This configuration overrides the CakePHP defaults locating in app.php
+     * Do not edit app.php as it may break your upgrade process
+     */
     'App' => [
         // A base URL to use for absolute links.
-        // Enter here the url where your passbolt will be reachable.
-        'fullBaseUrl' => 'https://enter-here-your-own-passbolt-url.com',
+        // The url where the passbolt instance will be reachable to your end users.
+        // This information is need to render images in emails for example
+        'fullBaseUrl' => 'https://www.passbolt.test',
     ],
 
     // Database configuration.
@@ -127,4 +70,77 @@ return [
             'url' => null,
         ],
     ],
+
+    /**
+     * DEFAULT PASSBOLT CONFIGURATION
+     *
+     * This is the default configuration.
+     * It enforces the use of ssl, and does not provide a default OpenPGP key.
+     * If your objective is to try passbolt quickly for evaluation purpose, and security is not important
+     * you can use the demo config example provided in the next section below.
+     */
+    'passbolt' => [
+        // Define if users can register by themselves.
+        'registration' => [
+            'public' => false,
+        ],
+        // Enforce the use of ssl
+        // requires additional webserver configuration and a SSL certificate
+        'ssl' => [
+            'force' => true,
+        ],
+        // GPG Configuration.
+        'gpg' => [
+            // Main server key.
+            'serverKey' => [
+                // Server private key fingerprint.
+                'fingerprint' => '',
+                // Server public / private key file location.
+                'public' => '',
+                'private' => '',
+            ],
+
+            // Replace the environment variable $GNUPGHOME with above value even if it is set.
+            // Useful if you do not want to use the default keyring location and cannot set environment variables.
+            // 'putenv' => true,
+
+            // Tell the application where to find the GnuPG keyring when passbolt.gpg.putenv is set to true.
+            // The keyring must to be owned and accessible by the webserver user.
+            // Example: www-data user on Debian
+            //
+            // The default location of the keyring can vary depending on your system:
+            // Apache on Centos: '/usr/share/httpd/.gnupg'
+            // Apache on Debian: '/var/www/.gnupg'
+            // Nginx on Centos: '/var/lib/nginx/.gnupg'
+            //
+            // 'keyring' => '',
+        ],
+    ],
+
+    /**
+     * DEMO CONFIGURATION EXAMPLE
+     *
+     * Uncomment the lines below if you want to try passbolt quickly.
+     * and if you are not concerned about the security of your installation.
+     * (Don't forget to comment the default config above).
+     */
+//    'debug' => true,
+//    'passbolt' => [
+//        'registration' => [
+//            'public' => true
+//        ],
+//        'ssl' => [
+//            'force' => false,
+//        ],
+//        'gpg' => [
+//            'serverKey' => [
+//                'fingerprint' => '2FC8945833C51946E937F9FED47B0811573EE67E',
+//                'public' => ROOT . DS . 'config' . DS . 'gpg' . DS . 'unsecure.key',
+//                'private' => ROOT . DS . 'config' . DS . 'gpg' . DS . 'unsecure_private.key',
+//            ],
+//            'putenv' => true,
+//            'keyring' => '/var/lib/nginx/.gnupg',
+//        ],
+//    ]
+
 ];
