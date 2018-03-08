24906e297745accd37665d17f1deba4aec70d48d
Kevin Muller <kevin@passbolt.com>
2018-03-08 20:31:47 +0530

    PASSBOLT-2694 compatibility for GPG V1 and V2

--- plugins/Passbolt/WebInstaller/src/Form/GpgKeyGenerateForm.php
+++ plugins/Passbolt/WebInstaller/src/Form/GpgKeyGenerateForm.php
@@ -80,24 +80,71 @@ class GpgKeyGenerateForm extends Form
         }
     }
 
-    /**
-     * Generate a key pair using system GPG binary.
-     * @param $keyData
-     * @return string
-     */
-    public function generateKey($keyData) {
-        $keyBatchFileContent = "Key-Type: 1
+	/**
+	 * Get server GPG version.
+	 * @return mixed
+	 */
+    public function getGpgVersion() {
+	    exec('gpg --version', $response, $res);
+	    preg_match("/((?:[0-9]+\.?)+)/i", $response[0], $matches);
+	    return $matches[0];
+    }
+
+	/**
+	 * Generate a key pair using system GPG binary V2.
+	 * @param $keyData
+	 * @return string
+	 */
+	public function generateKeyCmdV2($keyData) {
+		$cmd = "gpg --batch --no-tty --gen-key <<EOF
+Key-Type: default
+Key-Length: 2048
+Subkey-Type: default
+Subkey-Length: 2048
+Name-Real: {$keyData['name']}" . (isset($keyData['comment']) && !empty($keyData['comment']) ? "
+Name-Comment: {$keyData['comment']}" : '') . "
+Name-Email: {$keyData['email']}
+Expire-Date: 0
+%no-protection
+%commit
+EOF";
+
+		return $cmd;
+	}
+
+	/**
+	 * Generate a key pair using system GPG binary V1.
+	 * @param $keyData
+	 * @return string
+	 */
+	public function generateKeyCmdV1($keyData) {
+		$cmd = "gpg --batch --gen-key <<EOF
+Key-Type: 1
 Key-Length: 2048
 Name-Real: {$keyData['name']}" . (isset($keyData['comment']) && !empty($keyData['comment']) ? "
 Name-Comment: {$keyData['comment']}" : '') . "
 Name-Email: {$keyData['email']}
-Expire-Date: 0";
+Expire-Date: 0
+EOF";
 
-        $fileName = 'key_' . rand(10000, 99999) . '.txt';
-        $filePath = TMP . $fileName;
-        file_put_contents($filePath, $keyBatchFileContent);
+		echo $cmd; die();
 
-        $generateKeyCmd = "gpg --batch --gen-key $filePath";
+		return $cmd;
+	}
+
+    /**
+     * Generate a key pair using system GPG binary V1.
+     * @param $keyData
+     * @return string
+     */
+    public function generateKey($keyData) {
+	    $version = $this->getGpgVersion();
+	    $isV2 = version_compare($version, '2.0.0', '>=');
+	    if ($isV2) {
+		    $generateKeyCmd = $this->generateKeyCmdV2($keyData);
+	    } else {
+		    $generateKeyCmd = $this->generateKeyCmdV1($keyData);
+	    }
 
         $cmdOutput = "";
         exec($generateKeyCmd, $cmdOutput, $cmdRes);
@@ -112,9 +159,6 @@ Expire-Date: 0";
         if(empty($info) || !isset($info[0]['subkeys'][0]['fingerprint'])) {
             throw new Exception("Could not retrieve the generated key");
         }
-
-        // Delete previously created key batch file.
-        unlink($filePath);
 
         // There can be several keys that match the email id (already present in the keyring before).
         // We need to identify the last one.
