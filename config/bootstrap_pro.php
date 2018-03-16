f2bb8191a5b473c2a2dc237f3c4ee44817b43924
Kevin Muller <kevin@passbolt.com>
2018-03-16 21:00:05 +0100

    PASSBOLT-2511 passbolt pro bootstrap

--- config/bootstrap_pro.php
+++ config/bootstrap_pro.php
@@ -2,6 +2,7 @@
 // Add passbolt pro main plugin if present.
 if (file_exists(PLUGINS . DS . 'Passbolt' . DS . 'Pro')) {
 	Plugin::load('Passbolt/Pro', ['bootstrap' => true, 'routes' => false]);
+	define('PASSBOLT_PRO', true);
 }
 
 // Add other pro plugins below.
