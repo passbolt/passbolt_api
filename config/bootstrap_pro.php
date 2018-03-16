a92cf47f5bc00b7fbc23e02d284acd9c6c46e5fe
Kevin Muller <kevin@passbolt.com>
2018-03-16 21:47:09 +0100

    PASSBOLT-2511 make passbolt pro declare itself

--- config/bootstrap_pro.php
+++ config/bootstrap_pro.php
@@ -1,4 +1,6 @@
 <?php
+use Cake\Core\Plugin;
+
 // Add passbolt pro main plugin if present.
 if (file_exists(PLUGINS . DS . 'Passbolt' . DS . 'Pro')) {
 	Plugin::load('Passbolt/Pro', ['bootstrap' => true, 'routes' => false]);
