e0c464c4d2299ae4b3f65eb63f9af87a954fef6d
Cédric Alfonsi <cedric@passbolt.com>
2018-09-21 10:27:26 +0530

    Revert "PASSBOLT-2981 Throttle the resource save in case of Sql Deadlock in ResourceAddController."
    This reverts commit 0824f1d83b032226481c63591cfaa9e32175f039.

--- src/Model/Table/ResourcesTable.php
+++ src/Model/Table/ResourcesTable.php
@@ -19,7 +19,6 @@ use App\Error\Exception\CustomValidationException;
 use App\Model\Entity\Permission;
 use App\Model\Entity\Role;
 use App\Model\Rule\IsNotSoftDeletedRule;
-use App\Model\Traits\Model\ModelSaveTrait;
 use App\Model\Traits\Resources\ResourcesFindersTrait;
 use Cake\ORM\RulesChecker;
 use Cake\ORM\Table;
@@ -48,7 +47,6 @@ use Cake\Validation\Validator;
  */
 class ResourcesTable extends Table
 {
-    use ModelSaveTrait;
     use ResourcesFindersTrait;
 
     /**
