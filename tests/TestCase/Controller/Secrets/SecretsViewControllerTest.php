c7489eb163a81d19451780dee6e89f19af9a27aa
Cedric <cedricalfonsi@gmail.com>
2019-02-11 12:55:10 +0100

    Unit test the controller Secrets/SecretsView and the model function SecretsTable::findByResourceUser

---
+++ tests/TestCase/Controller/Secrets/SecretsViewControllerTest.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Passbolt ~ Open source password manager for teams
+ * Copyright (c) Passbolt SA (https://www.passbolt.com)
+ *
+ * Licensed under GNU Affero General Public License version 3 of the or any later version.
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Passbolt SA (https://www.passbolt.com)
+ * @license       https://opensource.org/licenses/AGPL-3.0 AGPL License
+ * @link          https://www.passbolt.com Passbolt(tm)
+ * @since         2.7.0
+ */
+
+namespace App\Test\TestCase\Controller\Secrets;
+
+use App\Test\Lib\AppIntegrationTestCase;
+use App\Utility\UuidFactory;
+use Cake\ORM\TableRegistry;
+
+class SecretsViewControllerTest extends AppIntegrationTestCase
+{
+    public $fixtures = ['app.Base/users', 'app.Base/secrets'];
+
+    public function testSuccess()
+    {
+        $this->authenticateAs('dame');
+        $resourceId = UuidFactory::uuid('resource.id.apache');
+        $this->getJson("/secrets/resource/$resourceId.json?api-version=2");
+        $this->assertSuccess();
+        $this->assertNotNull($this->_responseJsonBody);
+        $this->assertSecretAttributes($this->_responseJsonBody);
+    }
+
+    public function testErrorNotAuthenticated()
+    {
+        $resourceId = UuidFactory::uuid('resource.id.apache');
+        $this->getJson("/secrets/resource/$resourceId.json?api-version=2");
+        $this->assertAuthenticationError();
+    }
+
+    public function testErrorNotValidId()
+    {
+        $this->authenticateAs('dame');
+        $resourceId = 'invalid-id';
+        $this->getJson("/secrets/resource/$resourceId.json?api-version=2");
+        $this->assertError(400, 'The resource id is not valid.');
+    }
+
+    public function testErrorNotFound()
+    {
+        $this->authenticateAs('ada');
+        $resourceId = UuidFactory::uuid('resource.id.april');
+        $this->getJson("/secrets/resource/$resourceId.json?api-version=2");
+        $this->assertError(404, 'The secret does not exist.');
+    }
+}
