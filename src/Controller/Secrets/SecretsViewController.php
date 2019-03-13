a5e4c610ec711c7ce1d6a88b0458801d32a609c6
Remy Bertot <remy@passbolt.com>
2019-03-13 13:19:41 +0100

    Merge with latest commits on common

--- src/Controller/Secrets/SecretsViewController.php
+++ src/Controller/Secrets/SecretsViewController.php
@@ -16,8 +16,8 @@
 namespace App\Controller\Secrets;
 
 use App\Controller\AppController;
-use Cake\Network\Exception\BadRequestException;
-use Cake\Network\Exception\NotFoundException;
+use Cake\Http\Exception\BadRequestException;
+use Cake\Http\Exception\NotFoundException;
 use Cake\Validation\Validation;
 
 class SecretsViewController extends AppController
