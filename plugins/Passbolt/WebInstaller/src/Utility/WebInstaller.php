c3deb0f9f1b95f269ef848c44bdf40f26edae554
Remy Bertot <remy@passbolt.com>
2020-11-16 22:17:54 +0530

    Fix webinstaller isInitialized return type

--- plugins/Passbolt/WebInstaller/src/Utility/WebInstaller.php
+++ plugins/Passbolt/WebInstaller/src/Utility/WebInstaller.php
@@ -57,7 +57,7 @@ class WebInstaller
      */
     public function isInitialized(): bool
     {
-        return $this->getSettings('initialized');
+        return $this->getSettings('initialized') ?? false;
     }
 
     /**
