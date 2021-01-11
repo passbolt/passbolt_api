20463adeef0a25e64de8bfdf56f358d5bb56b55b
Kevin Muller <kevin@passbolt.com>
2021-01-11 18:35:29 +0000

    #CORE-121 Implement "As an AD using the web installer to configure Passbolt, I should see the database information pre-populated if they are known by the server"

--- plugins/Passbolt/WebInstaller/src/Form/DatabaseConfigurationForm.php
+++ plugins/Passbolt/WebInstaller/src/Form/DatabaseConfigurationForm.php
@@ -23,6 +23,12 @@ use Cake\Validation\Validator;
 class DatabaseConfigurationForm extends Form
 {
     /**
+     * Path of the ini config file.
+     * An ini config file can be placed in tmp and used to pre-populate the database configuration form.
+     */
+    public const CONFIG_FILE_PATH = TMP . 'db_credentials.ini';
+
+    /**
      * Database configuration schema.
      *
      * @param \Cake\Form\Schema $schema shchema
