bdeda9670fa8d23d9a67143dbe32c0a6728b5db3
Remy Bertot <remy@passbolt.com>
2021-01-19 20:14:57 +0530

    Fix QueryStringComponent normalize booleans should accept booleans

--- src/Controller/Component/QueryStringComponent.php
+++ src/Controller/Component/QueryStringComponent.php
@@ -570,6 +570,9 @@ class QueryStringComponent extends Component
      */
     public static function normalizeBoolean($str)
     {
+        if (is_bool($str)) {
+            return $str;
+        }
         if (!is_scalar($str)) {
             return false;
         }
