fcc62924258091a84822bec2d79d702210721498
Juan Pablo Ramirez <pabloelcolombiano@gmail.com>
2021-03-14 03:53:02 +0100

    PB-5441 Introduces a datasource option to all commands where this needed to be specify to run tests.

--- src/Command/MysqlImportCommand.php
+++ src/Command/MysqlImportCommand.php
@@ -23,18 +23,17 @@ use Cake\Datasource\ConnectionManager;
 
 class MysqlImportCommand extends PassboltCommand
 {
+    use DatabaseAwareCommandTrait;
+
     /**
      * @inheritDoc
      */
     public function buildOptionParser(ConsoleOptionParser $parser): ConsoleOptionParser
     {
-        $parser
-            ->setDescription(__('Utility to import a mysql database backups.'))
-            ->addOption('datasource', [
-                'short' => 'd',
-                'default' => 'default',
-                'help' => __('Datasource name'),
-            ])
+        $parser->setDescription(__('Utility to import a mysql database backups.'));
+
+        $this
+            ->addDatasourceOption($parser, false)
             ->addOption('file', [
                 'help' => 'The file to import the schema and data from',
                 'default' => null,
