3abf38d7d0fa56d1ff6452ec4015d83d87e8d5c8
Remy Bertot <remy@passbolt.com>
2022-10-04 17:17:01 +0200

    PB-19255 As an administrator I want to be able to trigger 500 errors on demand to test my logs

---
+++ src/Controller/Healthcheck/HealthcheckErrorController.php
@@ -0,0 +1,51 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * Passbolt ~ Open source password manager for teams
+ * Copyright (c) Passbolt SA (https://www.passbolt.com)
+ *
+ * Licensed under GNU Affero General Public License version 3 of the or any later version.
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Passbolt SA (https://www.passbolt.com)
+ * @license       https://opensource.org/licenses/AGPL-3.0 AGPL License
+ * @link          https://www.passbolt.com Passbolt(tm)
+ * @since         2.0.0
+ */
+namespace App\Controller\Healthcheck;
+
+use App\Controller\AppController;
+use Cake\Core\Configure;
+use Cake\Event\EventInterface;
+use Cake\Http\Exception\InternalErrorException;
+use Cake\Http\Exception\NotFoundException;
+
+class HealthcheckErrorController extends AppController
+{
+
+    /**
+     * @inheritDoc
+     */
+    public function beforeFilter(EventInterface $event)
+    {
+        if (Configure::read('passbolt.healthcheck.error') === true) {
+            $this->Authentication->allowUnauthenticated(['internal']);
+        } else {
+            throw new NotFoundException();
+        }
+    }
+
+    /**
+     * A lightweight methods that throws an internal error
+     * Used to test the error handling.
+     *
+     * @return void
+     * @throws InternalErrorException
+     */
+    public function internal(): void
+    {
+        throw new InternalErrorException();
+    }
+}
---
+++ tests/TestCase/Controller/Healthcheck/HealthcheckErrorControllerTest.php
@@ -0,0 +1,41 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * Passbolt ~ Open source password manager for teams
+ * Copyright (c) Passbolt SA (https://www.passbolt.com)
+ *
+ * Licensed under GNU Affero General Public License version 3 of the or any later version.
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Passbolt SA (https://www.passbolt.com)
+ * @license       https://opensource.org/licenses/AGPL-3.0 AGPL License
+ * @link          https://www.passbolt.com Passbolt(tm)
+ * @since         2.0.0
+ */
+namespace App\Test\TestCase\Controller\Healthcheck;
+
+use App\Test\Lib\AppIntegrationTestCase;
+use Cake\Core\Configure;
+
+class HealthcheckErrorControllerTest extends AppIntegrationTestCase
+{
+    public function testHealthcheckErrorDisabled()
+    {
+        $og = Configure::read('passbolt.healthcheck.error');
+        Configure::write('passbolt.healthcheck.error', false);
+        $this->getJson('/healthcheck/error.json');
+        $this->assertError(404);
+        Configure::write('passbolt.healthcheck.error', $og);
+    }
+
+    public function testHealthcheckErrorEnabled()
+    {
+        $og = Configure::read('passbolt.healthcheck.error');
+        Configure::write('passbolt.healthcheck.error', true);
+        $this->getJson('/healthcheck/error.json');
+        $this->assertError(500);
+        Configure::write('passbolt.healthcheck.error', $og);
+    }
+}
