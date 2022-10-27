a59fb4b22d043f645d6e3bd90e69f42c29a70fa9
Vincent Dumont <vincent@passbolt.com>
2022-10-27 09:46:37 +0200

    PB-19235 - Refactor Add Comments

--- src/Controller/Comments/CommentsAddController.php
+++ src/Controller/Comments/CommentsAddController.php
@@ -18,124 +18,27 @@ declare(strict_types=1);
 namespace App\Controller\Comments;
 
 use App\Controller\AppController;
-use App\Model\Entity\Comment;
-use Cake\Event\Event;
-use Cake\Http\Exception\BadRequestException;
-use Cake\Http\Exception\InternalErrorException;
-use Cake\Http\Exception\NotFoundException;
-use Cake\Utility\Hash;
-use Cake\Validation\Validation;
+use App\Service\Comments\CommentsAddService;
 
 /**
  * @property \App\Model\Table\CommentsTable $Comments
  */
 class CommentsAddController extends AppController
 {
-    public const ADD_SUCCESS_EVENT_NAME = 'CommentAddController.addPost.success';
-
     /**
      * Create a new comment for a resource.
      *
      * @param string $foreignKey The identifier of the resource to add a comment to
      * @throws \Cake\Http\Exception\BadRequestException
-     * @throws \Cake\Http\Exception\NotFoundException
      * @return void
      */
     public function addPost(string $foreignKey)
     {
-        if (!Validation::uuid($foreignKey)) {
-            throw new BadRequestException(__('The resource identifier should be a valid UUID.'));
-        }
-        $this->loadModel('Comments');
-
-        $comment = $this->_buildAndValidateCommentEntity($foreignKey);
-        $this->_handleValidationErrors($comment);
-
-        if (!$this->Comments->save($comment)) {
-            $this->_handleValidationErrors($comment);
-            $oops = __('Could not save the comment, please try again later.');
-            throw new InternalErrorException($oops);
-        }
-        $this->_notifyUsers($comment);
-        $this->success(__('The comment was successfully added.'), $comment);
-    }
-
-    /**
-     * Manage validation errors.
-     *
-     * @param \App\Model\Entity\Comment $comment comment
-     * @throws \Cake\Http\Exception\BadRequestException
-     * @throws \Cake\Http\Exception\NotFoundException
-     * @return void
-     */
-    protected function _handleValidationErrors(Comment $comment)
-    {
-        $errors = $comment->getErrors();
-        if (!empty($errors)) {
-            if (
-                !empty($errors['foreign_key']) &&
-                (
-                    !empty($errors['foreign_key']['resource_exists']) ||
-                 !empty($errors['foreign_key']['resource_is_soft_deleted']) ||
-                 !empty($errors['foreign_key']['has_resource_access'])
-                )
-            ) {
-                throw new NotFoundException(__('The resource does not exist.'));
-            }
-            throw new BadRequestException(__('Could not validate comment data.'));
-        }
-    }
-
-    /**
-     * Build and validate comment entity from user input.
-     *
-     * @param string|null $foreignKey The identifier of the instance the comment belongs to.
-     * @return \App\Model\Entity\Comment $comment comment entity
-     */
-    protected function _buildAndValidateCommentEntity(?string $foreignKey = null)
-    {
-        $data = $this->request->getData();
-
-        // Build entity and perform basic check.
-        $comment = $this->Comments->newEntity(
-            [
-                'user_id' => $this->User->id(),
-                'foreign_key' => $foreignKey,
-                'foreign_model' => 'Resource',
-                'parent_id' => Hash::get($data, 'parent_id'),
-                'content' => Hash::get($data, 'content'),
-                'created_by' => $this->User->id(),
-                'modified_by' => $this->User->id(),
-            ],
-            [
-                'accessibleFields' => [
-                    'user_id' => true,
-                    'foreign_key' => true,
-                    'foreign_model' => true,
-                    'parent_id' => true,
-                    'content' => true,
-                    'created_by' => true,
-                    'modified_by' => true,
-                ],
-            ]
+        $comment = (new CommentsAddService())->add(
+            $this->User->getAccessControl(),
+            $foreignKey,
+            $this->getRequest()->getData()
         );
-
-        $this->_handleValidationErrors($comment);
-
-        return $comment;
-    }
-
-    /**
-     * Notify users about this new comment
-     *
-     * @param \App\Model\Entity\Comment $comment comment entity
-     * @return void
-     */
-    protected function _notifyUsers(Comment $comment)
-    {
-        $event = new Event(static::ADD_SUCCESS_EVENT_NAME, $this, [
-            'comment' => $comment,
-        ]);
-        $this->getEventManager()->dispatch($event);
+        $this->success(__('The comment was successfully added.'), $comment);
     }
 }
