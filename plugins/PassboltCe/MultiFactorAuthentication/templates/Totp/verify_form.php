9d5bcd7fc6e1c2c3f12f9b572d6b54c0ab2813a3
Vincent Dumont <vincent@passbolt.com>
2022-12-09 14:11:03 +0100

    PB-21756 - Pass redirect parameter to other MFA providers

--- plugins/PassboltCe/MultiFactorAuthentication/templates/Totp/verify_form.php
+++ plugins/PassboltCe/MultiFactorAuthentication/templates/Totp/verify_form.php
@@ -29,6 +29,6 @@
         <input type="checkbox" name="remember" value="remember" id="remember">
         <label for="remember" ><?= __('Remember this device for a month.'); ?></label>
     </div>
-    <?= $this->element('formActions', ['providers' => $providers, 'currentProvider' => MfaSettings::PROVIDER_TOTP]); ?>
+    <?= $this->element('formActions', ['providers' => $providers, 'redirect' => $redirect, 'currentProvider' => MfaSettings::PROVIDER_TOTP]); ?>
     <?= $this->Form->end(); ?>
 </div>
