932e3ae3a2e591e9936d7ae08001dfad036f5cd1
Juan Pablo Ramirez <pabloelcolombiano@gmail.com>
2023-01-18 12:29:33 +0100

    PB-21924 Updates the release version

--- plugins/PassboltCe/SelfRegistration/config/routes.php
+++ plugins/PassboltCe/SelfRegistration/config/routes.php
@@ -11,7 +11,7 @@
  * @copyright     Copyright (c) Passbolt SA (https://www.passbolt.com)
  * @license       https://opensource.org/licenses/AGPL-3.0 AGPL License
  * @link          https://www.passbolt.com Passbolt(tm)
- * @since         3.9.0
+ * @since         3.10.0
  */
 use Cake\Routing\RouteBuilder;
 
