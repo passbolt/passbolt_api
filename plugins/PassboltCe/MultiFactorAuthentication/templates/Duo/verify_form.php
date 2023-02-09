aded73380c37ea64205de41736ad8f2be68113cf
Vincent Dumont <vincent@passbolt.com>
2023-02-09 09:42:51 +0100

    PB-21866 - As LU, I want to see a Duo verify form upon login

--- plugins/PassboltCe/MultiFactorAuthentication/templates/Duo/verify_form.php
+++ plugins/PassboltCe/MultiFactorAuthentication/templates/Duo/verify_form.php
@@ -8,18 +8,21 @@
     use Cake\Routing\Router;
     use Passbolt\MultiFactorAuthentication\Utility\MfaSettings;
 
-    $title = __('Duo multi-factor authentication');
+    $title = __('Multi factor authentication verification');
     $this->assign('title', $title);
     $this->assign('pageClass', 'login');
     $formContext = [
-        'url' => Router::url('/mfa/verify/duo?redirect=' . $redirect, true),
+        'url' => Router::url('/mfa/verify/duo/prompt?redirect=' . $redirect, true),
+        'target' => '_top',
         'id' => 'duo_form',
     ];
     ?>
 <div class="login-form ">
     <h1>
-        <?= $title; ?>
+        <?= __('Duo Multi Factor Authentication'); ?>
     </h1>
-    <?= $this->Form->create($verifyForm, $formContext); ?><?= $this->Form->end(); ?>
-    <?= $this->element('formActions', ['providers' => $providers, 'redirect' => $redirect, 'currentProvider' => MfaSettings::PROVIDER_DUO]); ?>
+    <?= $this->Form->create($verifyForm, $formContext); ?>
+        <button type="submit" class="button primary big full-width" style="margin-top:3.2rem;" role="button"><?= __('Sign-in with Duo'); ?></button>
+        <?= $this->element('formActions', ['providers' => $providers, 'redirect' => $redirect, 'currentProvider' => MfaSettings::PROVIDER_DUO]); ?>
+    <?= $this->Form->end(); ?>
 </div>
