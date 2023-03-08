439bd1cb2ab5c259a5e9ff695b29e8134a4bcf45
Cedric Alfonsi <cedric@passbolt.com>
2023-03-08 22:13:23 +0100

    PB-22091 Rename migration
    Signed-off-by: Cedric Alfonsi <cedric@passbolt.com>

--- config/Migrations/20230308124720_V3120DropActionLogsDuplicateIndexes.php
+++ config/Migrations/20230308124720_V3120DropActionLogsDuplicateIndexes.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 use Migrations\AbstractMigration;
 
-class V380DropActionLogsDuplicateIndexes extends AbstractMigration
+class V3120DropActionLogsDuplicateIndexes extends AbstractMigration
 {
     /**
      * Change Method.
