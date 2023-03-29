f058ebcd35261cb2f5f6bfb93478aad709f27ef6
Juan Pablo Ramirez <pabloelcolombiano@gmail.com>
2023-03-30 01:32:27 +0200

    PB-24073 Test extended

--- tests/TestCase/ChangelogFileTest.php
+++ tests/TestCase/ChangelogFileTest.php
@@ -24,79 +24,91 @@ use PHPUnit\Framework\TestCase;
  */
 class ChangelogFileTest extends TestCase
 {
+    public array $errors = [];
+
     public const SECTION_NAME_ALLOWED = [
         'Added',
+        'Changed',
         'Chores',
         'Fixed',
         'Improved',
         'Maintenance',
+        'Removed',
         'Security',
         'Security fixes',
     ];
 
-    public function testChangelogFile_Feature_File(): void
+    public function setUp(): void
     {
-        $file = $this->getFile(FIXTURES . 'Changelog' . DS . 'valid');
-        $pointer = 0;
-        $this->assertEquals('# Change Log', $file[$pointer]);
+        parent::setUp();
+        $this->errors = [];
+    }
 
-        // Jump to line 5 which has the tag number
-        $line5 = $file[4];
-        $this->assertEquals('## ', substr($line5, 0, 3));
-        $this->assertEquals('[', substr($line5, 3, 1));
-        $lastVersionLine = $line5;
-        preg_match('#(?<=\[)(.*?)(?=\])#', $lastVersionLine, $lastVersionInChangeLogs);
-        $tag = $lastVersionInChangeLogs[0];
-        $this->assertIsString($tag);
-        // The tag should not start with "v"
-        $this->assertNotSame('v', substr($tag, 0, 1));
-        $closingBracketPosition = strpos($line5, ']');
-        // Tag in square brackets and date shouold be separated by " - "
-        $this->assertEquals(' - ', substr($line5, $closingBracketPosition + 1, 3));
-        $date = substr($line5, $closingBracketPosition + 4, 10);
-        // The date should be in Y-M-d format
-        $parsedDate = FrozenDate::parseDate($date, 'Y-M-d')->toDateString();
-        $this->assertEquals($parsedDate, $date);
-        // End of the line
-        $this->assertSame($closingBracketPosition + 14, strlen($line5));
-
-        // Jump to line 6 where the changes start
-        $pointer = 5;
-        while (substr($file[$pointer], 0, 4) !== '## [') {
-            $pointer = $this->assertChangeBlock($file, $pointer);
-        }
+    public function tearDown(): void
+    {
+        parent::tearDown();
+        unset($this->errors);
     }
 
     public function testChangelogFile_ChangeLog_File(): void
     {
         $file = $this->getFile(ROOT . DS . 'CHANGELOG');
         if (!isset($file[4])) {
-            $this->markTestSkipped('No CHANGELOG.md file could be found');
+            $this->markTestSkipped('No valid CHANGELOG.md file could be found');
         }
+
+        $this->collectChangelogErrors($file);
+        $this->assertEmpty($this->errors);
+    }
+
+    public function testChangelogFile_Valid_Fixture_File(): void
+    {
+        $file = $this->getFile(FIXTURES . 'Changelog' . DS . 'valid');
+        $this->collectChangelogErrors($file);
+        $this->assertEmpty($this->errors);
+    }
+
+    public function testChangelogFile_InvalidFile(): void
+    {
+        $file = $this->getFile(FIXTURES . 'Changelog' . DS . 'invalid');
+        $this->collectChangelogErrors($file);
+        $this->assertSame(7, count($this->errors));
+    }
+
+    protected function collectChangelogErrors(array $file)
+    {
         $pointer = 0;
-        $this->assertEquals('# Change Log', $file[$pointer]);
+        $this->appendErrorIf($file[$pointer] !== '# Change Log', 'The first line should be "# Change Log"', $pointer);
 
         // Jump to line 5 which has the tag number
-        $line5 = $file[4];
-        $this->assertEquals('## ', substr($line5, 0, 3));
-        $this->assertEquals('[', substr($line5, 3, 1));
+        $pointer = 4;
+        $line5 = $file[$pointer];
+        $this->appendErrorIf(substr($line5, 0, 3) !== '## ', 'Tag line should start with ## ', $pointer);
+        $this->appendErrorIf(substr($line5, 3, 1) !== '[', 'Square bracket expected', $pointer);
+
+        // Assert that tag is well defined
         $lastVersionLine = $line5;
         preg_match('#(?<=\[)(.*?)(?=\])#', $lastVersionLine, $lastVersionInChangeLogs);
         $tag = $lastVersionInChangeLogs[0];
-        $this->assertIsString($tag);
-        // The tag should not start with "v"
-        $this->assertNotSame('v', substr($tag, 0, 1));
-        $closingBracketPosition = strpos($line5, ']');
-        // Tag in square brackets and date shouold be separated by " - "
-        $this->assertEquals(' - ', substr($line5, $closingBracketPosition + 1, 3));
-        $date = substr($line5, $closingBracketPosition + 4, 10);
-        // The date should be in Y-M-d format
-        $parsedDate = FrozenDate::parseDate($date, 'Y-M-d')->toDateString();
-        $this->assertEquals($parsedDate, $date);
-        // End of the line
-        $this->assertSame($closingBracketPosition + 14, strlen($line5));
+        $this->appendErrorIf(!is_string($tag), 'Tag could not be parsed', $pointer);
 
-        // Jump to line 6 where the changes start
+        // The tag should not start with "v"
+        $this->appendErrorIf(substr($tag, 0, 1) === 'v', 'Tag should not start with v', $pointer);
+
+        // Tag in square brackets and date should be separated by " - "
+        $closingBracketPosition = strpos($line5, ']');
+        $this->appendErrorIf(
+            substr($line5, $closingBracketPosition + 1, 3) !== ' - ',
+            'Tag and date should be separated by a -',
+            $pointer
+        );
+
+        // The date should be in Y-M-d format
+        $date = substr($line5, $closingBracketPosition + 4, 10);
+        $parsedDate = FrozenDate::parseDate($date, 'Y-M-d')->toDateString();
+        $this->appendErrorIf($parsedDate !== $date, 'The date could not be parsed', $pointer);
+
+        // Jump to line 6 where the changes block starts
         $pointer = 5;
         while (substr($file[$pointer], 0, 4) !== '## [') {
             $pointer = $this->assertChangeBlock($file, $pointer);
@@ -110,26 +122,36 @@ class ChangelogFileTest extends TestCase
 
     protected function assertChangeBlock(array $file, int $position): int
     {
-        $line1 = $file[$position];
-        $line1 = explode(' ', $line1);
-        $this->assertSame(2, count($line1));
-        $this->assertSame('###', $line1[0]);
-        $this->assertTrue(
-            in_array(
-                $line1[1],
-                self::SECTION_NAME_ALLOWED
-            ),
-            'The sections allowed are: ' . implode(', ', self::SECTION_NAME_ALLOWED)
-        );
+        $line = $file[$position];
+        $line = explode(' ', $line);
+        $this->appendErrorIf(count($line) !== 2, 'The change section should start with ### ', $position);
+        $this->appendErrorIf($line[0] !== '###', 'The change section should start with ### ', $position);
+        $this->appendErrorIf(!in_array(
+            $line[1],
+            self::SECTION_NAME_ALLOWED
+        ), 'The sections allowed are: ' . implode(', ', self::SECTION_NAME_ALLOWED), $position);
+
         $position++;
         while ($file[$position] !== '') {
             $line = $file[$position];
-            $this->assertSame('- ', substr($line, 0, 2), 'Each line should start with a dash and space');
-            $this->assertSame('PB-', substr($line, 2, 3), 'Each line should start with a ticket number');
+            $this->appendErrorIf(substr($line, 0, 2) !== '- ', 'Dash is missing', $position);
+            $this->appendErrorIf(
+                substr($line, 2, 3) !== 'PB-',
+                'Each line should start with a ticket number',
+                $position
+            );
             $position++;
         }
         $position++;
 
         return $position;
+    }
+
+    protected function appendErrorIf(bool $condition, string $msg, int $pointer): void
+    {
+        $pointer++;
+        if ($condition) {
+            $this->errors[] = $msg . " :: line #{$pointer}";
+        }
     }
 }
