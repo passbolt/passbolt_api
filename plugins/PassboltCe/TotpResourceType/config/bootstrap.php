1bd03ee521bfac5b4b8851776606c218d097d44c
Ishan Vyas <ishan@passbolt.com>
2023-04-21 11:20:06 +0530

    PB-24245 - Remove useless condition
    Plugin is enabled or not condition not required because if the plugin is enabled, then its config should be read.

--- plugins/PassboltCe/TotpResourceType/config/bootstrap.php
+++ plugins/PassboltCe/TotpResourceType/config/bootstrap.php
@@ -2,7 +2,4 @@
 
 use Cake\Core\Configure;
 
-$pluginEnabled = Configure::read('passbolt.plugins.toptResourceType.enabled');
-if (!isset($pluginEnabled) || $pluginEnabled === true) {
-    Configure::load('Passbolt/TotpResourceType.config', 'default', true);
-}
+Configure::load('Passbolt/TotpResourceType.config', 'default', true);
