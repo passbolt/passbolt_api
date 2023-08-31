e51276811acdd08ec0b7108ccbdc364ed8270cca
Juan Pablo Ramirez <pabloelcolombiano@gmail.com>
2023-08-31 16:13:45 +0200

    PB-25497 Fixers a test failing after merge with common

--- tests/TestCase/Controller/Notifications/ResourcesUpdateNotificationTest.php
+++ tests/TestCase/Controller/Notifications/ResourcesUpdateNotificationTest.php
@@ -91,42 +91,4 @@ class ResourcesUpdateNotificationTest extends AppIntegrationTestCase
         // check email notification
         $this->assertEmailQueueIsEmpty();
     }
-
-    public function testResourcesUpdateNotificationSuccess_Secrets(): void
-    {
-        $this->setEmailNotificationSetting('send.password.update', true);
-        // Get and update resource
-        $resourceId = UuidFactory::uuid('resource.id.apache');
-        // Prepare secrets
-        $bettyId = UuidFactory::uuid('user.id.betty');
-        $bettyEncryptedSecret = $this->encryptMessageFor($bettyId, 'R1 secret updated');
-        $carolId = UuidFactory::uuid('user.id.carol');
-        $carolEncryptedSecret = $this->encryptMessageFor($carolId, 'R1 secret updated');
-        $dameId = UuidFactory::uuid('user.id.dame');
-        $dameEncryptedSecret = $this->encryptMessageFor($dameId, 'R1 secret updated');
-        $adaId = UuidFactory::uuid('user.id.ada');
-        $adaEncryptedSecret = $this->encryptMessageFor($adaId, 'R1 secret updated');
-        $data = [
-            'name' => 'R1 name updated',
-            'username' => 'R1 username updated',
-            'uri' => 'https://r1-updated.com',
-            'description' => 'R1 description updated',
-            'secrets' => [
-                ['user_id' => $bettyId, 'data' => $bettyEncryptedSecret],
-                ['user_id' => $carolId, 'data' => $carolEncryptedSecret],
-                ['user_id' => $dameId, 'data' => $dameEncryptedSecret],
-                ['user_id' => $adaId, 'data' => $adaEncryptedSecret],
-            ],
-        ];
-        $this->authenticateAs('betty');
-
-        $this->putJson("/resources/{$resourceId}.json", $data);
-
-        $this->assertSuccess();
-        // Assert email contents
-        $this->assertEmailInBatchContains('updated the password', 'ada@passbolt.com');
-        $this->assertEmailInBatchContains('updated the password', 'betty@passbolt.com');
-        $this->assertEmailInBatchContains('updated the password', 'carol@passbolt.com');
-        $this->assertEmailInBatchContains('updated the password', 'dame@passbolt.com');
-    }
 }
