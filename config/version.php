d27570a6e601d40695c1703b368031034e974025
Juan Pablo Ramirez <pabloelcolombiano@gmail.com>
2023-11-28 00:38:53 +0100

    PB-28918 v4.4.2

--- config/version.php
+++ config/version.php
@@ -1,8 +1,8 @@
 <?php
 return [
     'passbolt' => [
-        'version' => '4.4.2-test.1',
-        'name' => 'Is It Because I\'m Black',
+        'version' => '4.4.2',
+        'name' => 'Is It Because I\'m Black?',
     ],
     'php' => [
         'minVersion' => '7.4',
