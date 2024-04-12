39c3ae1284ed22b159bc5ec070d3861cde33e13d
Diego Lendoiro <diego@passbolt.com>
2024-04-12 09:38:57 +0000

    PB-32987: update target branch for release notes MR

--- .gitlab-ci/scripts/bin/help_site.sh
+++ .gitlab-ci/scripts/bin/help_site.sh
@@ -14,7 +14,7 @@ GIT_CI_TOKEN_NAME=${GIT_CI_TOKEN_NAME:-gitlab-ci-token}
 ACCESS_TOKEN_NAME="help-site-bot"
 HELP_SITE_REPO="gitlab.com/passbolt/passbolt-help.git"
 RELEASE_NOTES_PATH="../RELEASE_NOTES.md"
-
+TARGET_MR_BRANCH="develop"
 
 function create_release_notes() {
     title="$(grep name ../config/version.php | awk -F "'" '{print $4}')"
@@ -25,7 +25,7 @@ function create_release_notes() {
     permalink="/releases/$PASSBOLT_FLAVOUR/$(grep name ../config/version.php | awk -F "'" '{print $4}' | tr ' ' '_' | tr '[:upper:]' '[:lower:]')"
     date="$(date +'%Y-%m-%d')"
 
-    cat << EOF >> _releases/"$PASSBOLT_FLAVOUR"/"$CI_COMMIT_TAG".md
+    cat <<EOF >>_releases/"$PASSBOLT_FLAVOUR"/"$CI_COMMIT_TAG".md
 ---
 title: $title
 slug: $slug
@@ -40,7 +40,7 @@ date: $date
 ---
 EOF
 
-    cat $RELEASE_NOTES_PATH >> _releases/"$PASSBOLT_FLAVOUR"/"$CI_COMMIT_TAG".md
+    cat $RELEASE_NOTES_PATH >>_releases/"$PASSBOLT_FLAVOUR"/"$CI_COMMIT_TAG".md
 }
 
 setup_gpg_key "$GPG_KEY_PATH" "$GPG_PASSPHRASE" "$GPG_KEY_GRIP"
@@ -55,6 +55,6 @@ git checkout -b release_notes_"$CI_COMMIT_TAG"_"$PASSBOLT_FLAVOUR"
 git add _releases/"$PASSBOLT_FLAVOUR"/"$CI_COMMIT_TAG".md
 git commit -m ":robot: Automatically added release notes for version $CI_COMMIT_TAG $PASSBOLT_FLAVOUR"
 glab auth login --token "$HELPSITE_TOKEN"
-mr_url=$(glab mr create -s release_notes_"$CI_COMMIT_TAG"_"$PASSBOLT_FLAVOUR" -b master -d ":robot: Release notes for $CI_COMMIT_TAG $PASSBOLT_FLAVOUR" -t "Release notes for $PASSBOLT_FLAVOUR $CI_COMMIT_TAG" --push --repo "passbolt/passbolt-help" | grep 'https://gitlab.com/passbolt/passbolt-help/-/merge_requests/')
+mr_url=$(glab mr create -s release_notes_"$CI_COMMIT_TAG"_"$PASSBOLT_FLAVOUR" -b "$TARGET_MR_BRANCH" -d ":robot: Release notes for $CI_COMMIT_TAG $PASSBOLT_FLAVOUR" -t "Release notes for $PASSBOLT_FLAVOUR $CI_COMMIT_TAG" --push --repo "passbolt/passbolt-help" | grep 'https://gitlab.com/passbolt/passbolt-help/-/merge_requests/')
 cd -
 bash .gitlab-ci/scripts/bin/slack-status-messages.sh ":notebook: New helpsite release notes created for $CI_COMMIT_TAG $PASSBOLT_FLAVOUR" "$mr_url"
