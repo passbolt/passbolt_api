78ffe22c1febf53a4fcaba0956fbd9ee7be5f0fd
Ishan Vyas <ishan@passbolt.com>
2024-05-14 09:50:19 +0000

    PB-32579 Improve tests logic to fix tests failing abruptly

--- plugins/PassboltCe/EmailDigest/tests/Factory/EmailQueueFactory.php
+++ plugins/PassboltCe/EmailDigest/tests/Factory/EmailQueueFactory.php
@@ -55,7 +55,7 @@ class EmailQueueFactory extends CakephpBaseFactory
 
             return [
                 'email' => $email,
-                'subject' => $faker->sentence(),
+                'subject' => $faker->sentence(3),
                 'config' => 'default',
                 'template' => 'test_email',
                 'layout' => 'default',
