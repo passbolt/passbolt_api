5dd9fbcfc59729a1b94294f809c2da19f32765cf
Ishan Vyas <ishan@passbolt.com>
2024-08-21 11:06:22 +0530

    PB-34456 Create metadata_keys table

---
+++ config/Migrations/20240820111234_V4100CreateMetadataKeys.php
@@ -0,0 +1,90 @@
+<?php
+declare(strict_types=1);
+/**
+ * Passbolt ~ Open source password manager for teams
+ * Copyright (c) Passbolt SA (https://www.passbolt.com)
+ *
+ * Licensed under GNU Affero General Public License version 3 of the or any later version.
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Passbolt SA (https://www.passbolt.com)
+ * @license       https://opensource.org/licenses/AGPL-3.0 AGPL License
+ * @link          https://www.passbolt.com Passbolt(tm)
+ * @since         4.10.0
+ */
+// @codingStandardsIgnoreStart
+use Cake\Core\Configure;
+use Migrations\AbstractMigration;
+
+class V4100CreateMetadataKeys extends AbstractMigration
+{
+    /**
+     * @inheritDoc
+     */
+    public function shouldExecute(): bool
+    {
+        return Configure::read('passbolt.v5.enabled');
+    }
+
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     * @return void
+     */
+    public function change(): void
+    {
+        $this
+            ->table('metadata_keys', ['id' => false, 'primary_key' => ['id'], 'collation' => 'utf8mb4_unicode_ci'])
+            ->addColumn('id', 'char', [
+                'default' => null,
+                'limit' => 36,
+                'null' => false,
+                'collation' => 'ascii_general_ci',
+            ])
+            ->addColumn('fingerprint', 'string', [
+                'default' => null,
+                'limit' => 51,
+                'null' => false,
+            ])
+            ->addColumn('armored_key', 'text', [
+                'default' => null,
+                'null' => false,
+            ])
+            ->addColumn('created', 'datetime', [
+                'default' => null,
+                'limit' => null,
+                'null' => false,
+            ])
+            ->addColumn('modified', 'datetime', [
+                'default' => null,
+                'limit' => null,
+                'null' => false,
+            ])
+            ->addColumn('deleted', 'datetime', [
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+            ])
+            ->addColumn('created_by', 'char', [
+                'default' => null,
+                'limit' => 36,
+                'null' => false,
+                'collation' => 'ascii_general_ci', // required for FK, needs to be same as reference table (i.e. users)
+            ])
+            ->addColumn('modified_by', 'char', [
+                'default' => null,
+                'limit' => 36,
+                'null' => false,
+                'collation' => 'ascii_general_ci', // required for FK, needs to be same as reference table (i.e. users)
+            ])
+            ->addIndex(['created_by'])
+            ->addIndex(['modified_by'])
+            ->addForeignKey('created_by', 'users', 'id', ['delete'=> 'NO_ACTION', 'update'=> 'NO_ACTION'])
+            ->addForeignKey('modified_by', 'users', 'id', ['delete'=> 'NO_ACTION', 'update'=> 'NO_ACTION'])
+            ->create();
+    }
+}
+// @codingStandardsIgnoreEnd
