70d272d9f502e0440048ca7e138a8c1cc6db37fc
Ishan Vyas <ishan@passbolt.com>
2024-08-21 12:40:44 +0530

    PB-34450 Update resources table with metadata fields

---
+++ config/Migrations/20240821052536_V4100AddMetadataFieldsToResources.php
@@ -0,0 +1,64 @@
+<?php
+declare(strict_types=1);
+/**
+ * Passbolt ~ Open source password manager for teams
+ * Copyright (c) Passbolt SA (https://www.passbolt.com)
+ *
+ * Licensed under GNU Affero General Public License version 3 of the or any later version.
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Passbolt SA (https://www.passbolt.com)
+ * @license       https://opensource.org/licenses/AGPL-3.0 AGPL License
+ * @link          https://www.passbolt.com Passbolt(tm)
+ * @since         4.10.0
+ */
+// @codingStandardsIgnoreStart
+use Cake\Core\Configure;
+use Migrations\AbstractMigration;
+use Phinx\Db\Adapter\MysqlAdapter;
+
+class V4100AddMetadataFieldsToResources extends AbstractMigration
+{
+    /**
+     * @inheritDoc
+     */
+    public function shouldExecute(): bool
+    {
+        return Configure::read('passbolt.v5.enabled');
+    }
+
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     * @return void
+     */
+    public function change(): void
+    {
+        $this
+            ->table('resources')
+            ->addColumn('metadata', 'text', [
+                'default' => null,
+                'limit' => MysqlAdapter::TEXT_MEDIUM,
+                'null' => true,
+                'after' => 'description',
+            ])
+            ->addColumn('metadata_key_id', 'char', [
+                'default' => null,
+                'limit' => 36,
+                'null' => true,
+                'after' => 'metadata',
+                'collation' => 'ascii_general_ci',
+            ])
+            ->addColumn('metadata_key_type', 'string', [
+                'default' => null,
+                'limit' => 100,
+                'null' => true,
+                'after' => 'metadata_key_id',
+            ])
+            ->update();
+    }
+}
+// @codingStandardsIgnoreEnd
