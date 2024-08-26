c774e36e8d84e3df6f2c96acedc15fbedf86f830
ABHISHEK TIWARI <tiwari.abhishektiwari23@gmail.com>
2024-08-26 07:59:31 +0000

    PB-33332 Refactor ResourcesUpdateControllerTest to use Fixture Factories

--- tests/TestCase/Controller/Resources/ResourcesUpdateControllerTest.php
+++ tests/TestCase/Controller/Resources/ResourcesUpdateControllerTest.php
@@ -18,16 +18,9 @@ declare(strict_types=1);
 namespace App\Test\TestCase\Controller\Resources;
 
 use App\Model\Entity\Permission;
-use App\Test\Fixture\Base\FavoritesFixture;
-use App\Test\Fixture\Base\GpgkeysFixture;
-use App\Test\Fixture\Base\GroupsFixture;
-use App\Test\Fixture\Base\GroupsUsersFixture;
-use App\Test\Fixture\Base\PermissionsFixture;
-use App\Test\Fixture\Base\ProfilesFixture;
-use App\Test\Fixture\Base\ResourcesFixture;
-use App\Test\Fixture\Base\RolesFixture;
-use App\Test\Fixture\Base\SecretsFixture;
-use App\Test\Fixture\Base\UsersFixture;
+use App\Test\Factory\ResourceFactory;
+use App\Test\Factory\RoleFactory;
+use App\Test\Factory\UserFactory;
 use App\Test\Lib\AppIntegrationTestCase;
 use App\Test\Lib\Model\GroupsModelTrait;
 use App\Test\Lib\Model\SecretsModelTrait;
@@ -38,23 +31,11 @@ class ResourcesUpdateControllerTest extends AppIntegrationTestCase
     use GroupsModelTrait;
     use SecretsModelTrait;
 
-    public $fixtures = [
-        FavoritesFixture::class,
-        GpgkeysFixture::class,
-        GroupsFixture::class,
-        GroupsUsersFixture::class,
-        PermissionsFixture::class,
-        ProfilesFixture::class,
-        ResourcesFixture::class,
-        RolesFixture::class,
-        SecretsFixture::class,
-        UsersFixture::class,
-    ];
-
     public function testUpdateResourcesController_Success_UpdateResourceMeta(): void
     {
-        [$r1, $userAId, $userBId] = $this->insertFixture_UpdateResourceMeta();
-        $this->authenticateAs('betty');
+        RoleFactory::make()->guest()->persist();
+        [$r1, $userA, $userB] = $this->insertFixture_UpdateResourceMeta();
+        $this->logInAs($userB);
 
         $data = [
             'name' => 'R1 name updated',
@@ -74,18 +55,18 @@ class ResourcesUpdateControllerTest extends AppIntegrationTestCase
         $this->assertEquals($data['username'], $resource->username);
         $this->assertEquals($data['uri'], $resource->uri);
         $this->assertEquals($data['description'], $resource->description);
-        $this->assertEquals($userAId, $resource->created_by);
-        $this->assertEquals($userBId, $resource->modified_by);
+        $this->assertEquals($userA->id, $resource->created_by);
+        $this->assertEquals($userB->id, $resource->modified_by);
 
         // Check the creator attribute
         $this->assertNotNull($resource->creator);
         $this->assertUserAttributes($resource->creator);
-        $this->assertEquals($userAId, $resource->creator->id);
+        $this->assertEquals($userA->id, $resource->creator->id);
 
         // Check the modifier attribute
         $this->assertNotNull($resource->modifier);
         $this->assertUserAttributes($resource->modifier);
-        $this->assertEquals($userBId, $resource->modifier->id);
+        $this->assertEquals($userB->id, $resource->modifier->id);
 
         // Check the secrets attribute
         // Only the logged-in user secrets should be returned.
@@ -96,30 +77,30 @@ class ResourcesUpdateControllerTest extends AppIntegrationTestCase
 
     private function insertFixture_UpdateResourceMeta(): array
     {
-        // Ada is OWNER of resource R1
-        // Betty is OWNER of resource R1
+        // UserA aka Ada is OWNER of resource R1
+        // UserB aka Betty is OWNER of resource R1
         // ---
         // R1 (Ada:O, Betty:O)
-        $userAId = UuidFactory::uuid('user.id.ada');
-        $userBId = UuidFactory::uuid('user.id.betty');
-        $r1 = $this->addResourceFor(['name' => 'R1', 'username' => 'R1 username', 'uri' => 'https://r1.com', 'description' => 'R1 description'], [$userAId => Permission::OWNER, $userBId => Permission::OWNER]);
+        [$userA, $userB] = UserFactory::make(2)->user()->persist();
 
-        return [$r1, $userAId, $userBId];
+        $r1 = $this->addResourceFor(['name' => 'R1', 'username' => 'R1 username', 'uri' => 'https://r1.com', 'description' => 'R1 description'], [$userA->id => Permission::OWNER, $userB->id => Permission::OWNER]);
+
+        return [$r1, $userA, $userB];
     }
 
     public function testUpdateResourcesController_Success_UpdateResourceSecrets(): void
     {
-        [$r1, $g1, $userAId, $userBId, $userCId] = $this->insertFixture_UpdateResourceSecrets();
-        $this->authenticateAs('betty');
-
-        $r1EncryptedSecretA = $this->encryptMessageFor($userAId, 'R1 secret updated');
-        $r1EncryptedSecretB = $this->encryptMessageFor($userBId, 'R1 secret updated');
-        $r1EncryptedSecretC = $this->encryptMessageFor($userCId, 'R1 secret updated');
+        RoleFactory::make()->guest()->persist();
+        [$r1, $g1, $userA, $userB, $userC] = $this->insertFixture_UpdateResourceSecrets();
+        $this->logInAs($userB);
+        $r1EncryptedSecretA = $this->encryptMessageFor($userA->id, 'R1 secret updated');
+        $r1EncryptedSecretB = $this->encryptMessageFor($userB->id, 'R1 secret updated');
+        $r1EncryptedSecretC = $this->encryptMessageFor($userC->id, 'R1 secret updated');
         $data = [
             'secrets' => [
-                ['user_id' => $userAId, 'data' => $r1EncryptedSecretA],
-                ['user_id' => $userBId, 'data' => $r1EncryptedSecretB],
-                ['user_id' => $userCId, 'data' => $r1EncryptedSecretC],
+                ['user_id' => $userA->id, 'data' => $r1EncryptedSecretA],
+                ['user_id' => $userB->id, 'data' => $r1EncryptedSecretB],
+                ['user_id' => $userC->id, 'data' => $r1EncryptedSecretC],
             ],
         ];
         $this->putJson("/resources/$r1->id.json", $data);
@@ -134,18 +115,18 @@ class ResourcesUpdateControllerTest extends AppIntegrationTestCase
         $this->assertEquals($r1->username, $resourceUpdated->username);
         $this->assertEquals($r1->uri, $resourceUpdated->uri);
         $this->assertEquals($r1->description, $resourceUpdated->description);
-        $this->assertEquals($userAId, $resourceUpdated->created_by);
-        $this->assertEquals($userBId, $resourceUpdated->modified_by);
+        $this->assertEquals($userA->id, $resourceUpdated->created_by);
+        $this->assertEquals($userB->id, $resourceUpdated->modified_by);
 
         // Check the creator attribute
         $this->assertNotNull($resourceUpdated->creator);
         $this->assertUserAttributes($resourceUpdated->creator);
-        $this->assertEquals($userAId, $resourceUpdated->creator->id);
+        $this->assertEquals($userA->id, $resourceUpdated->creator->id);
 
         // Check the modifier attribute
         $this->assertNotNull($resourceUpdated->modifier);
         $this->assertUserAttributes($resourceUpdated->modifier);
-        $this->assertEquals($userBId, $resourceUpdated->modifier->id);
+        $this->assertEquals($userB->id, $resourceUpdated->modifier->id);
 
         // Check the secrets attribute
         // Only the logged-in user secrets should be returned.
@@ -162,21 +143,19 @@ class ResourcesUpdateControllerTest extends AppIntegrationTestCase
         // G1 is OWNER of resource R1
         // ---
         // R1 (Ada:O, Betty:O, G1:O)
-        $userAId = UuidFactory::uuid('user.id.ada');
-        $userBId = UuidFactory::uuid('user.id.betty');
-        $userCId = UuidFactory::uuid('user.id.carol');
+        [$userA, $userB, $userC] = UserFactory::make(3)->withValidGpgKey()->persist();
         $g1 = $this->addGroup(['name' => 'G1', 'groups_users' => [
-            ['user_id' => $userAId, 'is_admin' => true],
-            ['user_id' => $userCId, 'is_admin' => true],
+            ['user_id' => $userA->id, 'is_admin' => true],
+            ['user_id' => $userC->id, 'is_admin' => true],
         ]]);
-        $r1 = $this->addResourceFor(['name' => 'R1', 'username' => 'R1 username', 'uri' => 'https://r1.com', 'description' => 'R1 description'], [$userAId => Permission::OWNER, $userBId => Permission::OWNER], [$g1->id => Permission::OWNER]);
+        $r1 = $this->addResourceFor(['name' => 'R1', 'username' => 'R1 username', 'uri' => 'https://r1.com', 'description' => 'R1 description'], [$userA->id => Permission::OWNER, $userB->id => Permission::OWNER], [$g1->id => Permission::OWNER]);
 
-        return [$r1, $g1, $userAId, $userBId, $userCId];
+        return [$r1, $g1, $userA, $userB, $userC];
     }
 
     public function testUpdateResourcesController_Error_NotValidId(): void
     {
-        $this->authenticateAs('ada');
+        $this->logInAsUser();
         $resourceId = 'invalid-id';
         $this->putJson("/resources/$resourceId.json");
         $this->assertError(400, 'The resource identifier should be a valid UUID.');
@@ -184,8 +163,8 @@ class ResourcesUpdateControllerTest extends AppIntegrationTestCase
 
     public function testUpdateResourcesController_Error_ValidationErrors(): void
     {
-        [$r1, $userAId, $userBId] = $this->insertFixture_UpdateResourceMeta();
-        $this->authenticateAs('ada');
+        [$r1, $userA, $userB] = $this->insertFixture_UpdateResourceMeta();
+        $this->logInAs($userA);
 
         $data = [
             'name' => '',
@@ -197,7 +176,7 @@ class ResourcesUpdateControllerTest extends AppIntegrationTestCase
     public function testUpdateResourcesController_Error_CsrfToken(): void
     {
         $this->disableCsrfToken();
-        $this->authenticateAs('ada');
+        $this->logInAsUser();
         $resourceId = UuidFactory::uuid();
         $this->put("/resources/$resourceId.json");
         $this->assertResponseCode(403);
@@ -205,11 +184,11 @@ class ResourcesUpdateControllerTest extends AppIntegrationTestCase
 
     public function testUpdateResourcesController_Error_InsufficientPermission(): void
     {
-        [$r1, $userAId, $userBId] = $this->insertFixture_InsufficientPermission();
+        [$r1, $userA, $userB] = $this->insertFixture_InsufficientPermission();
         $data = [
             'name' => ['Updated name'],
         ];
-        $this->authenticateAs('betty');
+        $this->logInAs($userB);
         $this->putJson("/resources/$r1->id.json", $data);
         $this->assertError(403, 'You are not allowed to update this resource.');
     }
@@ -220,23 +199,23 @@ class ResourcesUpdateControllerTest extends AppIntegrationTestCase
         // Betty has READ on resource R1
         // ---
         // R1 (Ada:O, Betty:R)
-        $userAId = UuidFactory::uuid('user.id.ada');
-        $userBId = UuidFactory::uuid('user.id.betty');
-        $r1 = $this->addResourceFor(['name' => 'R1'], [$userAId => Permission::OWNER, $userBId => Permission::READ]);
+        [$userA, $userB] = UserFactory::make(2)->user()->persist();
 
-        return [$r1, $userAId, $userBId];
+        $r1 = $this->addResourceFor(['name' => 'R1'], [$userA->id => Permission::OWNER, $userB->id => Permission::READ]);
+
+        return [$r1, $userA, $userB];
     }
 
     public function testUpdateResourcesController_Error_NotAuthenticated(): void
     {
-        [$r1, $userAId, $userBId] = $this->insertFixture_InsufficientPermission();
-        $this->putJson("/resources/$r1->id.json", []);
+        $resourceId = UuidFactory::uuid();
+        $this->putJson("/resources/$resourceId.json", []);
         $this->assertAuthenticationError();
     }
 
     public function testUpdateResourcesController_Error_ResourceDoesNotExist(): void
     {
-        $this->authenticateAs('ada');
+        $this->logInAsUser();
         $resourceId = UuidFactory::uuid();
         $this->putJson("/resources/$resourceId.json");
         $this->assertError(404, 'The resource does not exist.');
@@ -244,16 +223,16 @@ class ResourcesUpdateControllerTest extends AppIntegrationTestCase
 
     public function testUpdateResourcesController_Error_NoAccessToResource(): void
     {
-        [$r1, $userAId, $userBId] = $this->insertFixture_InsufficientPermission();
-        $this->authenticateAs('dame');
+        [$r1, $userA, $userB] = $this->insertFixture_InsufficientPermission();
+        $this->logInAsUser();
         $this->putJson("/resources/$r1->id.json");
         $this->assertError(404, 'The resource does not exist.');
     }
 
     public function testUpdateResourcesController_Error_ResourceIsSoftDeleted(): void
     {
-        $this->authenticateAs('ada');
-        $resourceId = UuidFactory::uuid('resource.id.jquery');
+        $this->logInAsUser();
+        $resourceId = ResourceFactory::make()->deleted()->persist()->id;
         $this->putJson("/resources/$resourceId.json", []);
         $this->assertError(404, 'The resource does not exist.');
     }
@@ -263,16 +242,15 @@ class ResourcesUpdateControllerTest extends AppIntegrationTestCase
      */
     public function testUpdateResourcesController_Error_NotJson(): void
     {
-        [$r1, $userAId, $userBId] = $this->insertFixture_UpdateResourceMeta();
-        $this->authenticateAs('betty');
-
+        $this->logInAsUser();
+        $id = UuidFactory::uuid();
         $data = [
             'name' => 'R1 name updated',
             'username' => 'R1 username updated',
             'uri' => 'https://r1-updated.com',
             'description' => 'R1 description updated',
         ];
-        $this->put("/resources/$r1->id", $data);
+        $this->put("/resources/$id", $data);
         $this->assertResponseCode(404);
     }
 }
