2947bf9526e6591d1d697cf5f1a20bf09a008768
Ishan Vyas <ishan@passbolt.com>
2025-03-07 14:09:40 +0530

    PB-36969 Update remaining code to use add all() before count()

--- tests/TestCase/Command/InstallCommandTest.php
+++ tests/TestCase/Command/InstallCommandTest.php
@@ -108,7 +108,7 @@ class InstallCommandTest extends AppTestCase
 
         $this->assertSame(
             1,
-            TableRegistry::getTableLocator()->get('Avatars')->find()->count()
+            TableRegistry::getTableLocator()->get('Avatars')->find()->all()->count()
         );
     }
 
