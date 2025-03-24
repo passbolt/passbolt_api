a558d5a8add1c8bb8a1677decdc1f9d9ed6d2d8e
Juan Pablo Ramirez <pabloelcolombiano@gmail.com>
2025-03-24 22:30:44 +0100

    PB-40263 Clear cache prior to running tests
    PB-40263 Reduce cache time

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -105,7 +105,7 @@ Configure::write('App.paths', [
  * for a short time.
  */
 if (Configure::read('debug')) {
-    Configure::write('Cache._cake_model_.duration', '+2 minutes');
+    Configure::write('Cache._cake_model_.duration', '+1 millisecond');
     Configure::write('Cache._cake_core_.duration', '+2 minutes');
 }
 
