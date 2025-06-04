c78fe7cb641288c49cbc2d7785451c3e326d12f4
Ishan Vyas <ishan@passbolt.com>
2025-06-04 16:51:21 +0530

    PB-43115 Fix XSS in email where URL in footer is not escaped or sanitized

--- templates/layout/email/html/default.php
+++ templates/layout/email/html/default.php
@@ -164,7 +164,7 @@ http://templates.mailchimp.com/development/css/outlook-conditional-css
                                                     <table align="Left" border="0" cellpadding="0" cellspacing="0" width="160" class="flexibleContainer" style="border-collapse: collapse;mso-table-lspace: 0pt;mso-table-rspace: 0pt;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;">
                                                         <tr>
                                                             <td align="Left" valign="top" class="imageContent" style="mso-table-lspace: 0pt;mso-table-rspace: 0pt;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;padding-bottom: 0px;">
-                                                                <img src="<?php echo $fullBaseUrl . '/img/logo/logo.png'?>" width="160" class="flexibleImage" style="max-width: 160px;border: 0;outline: none;text-decoration: none;-ms-interpolation-mode: bicubic;height: auto;">
+                                                                <img src="<?php echo Purifier::clean($fullBaseUrl) . '/img/logo/logo.png'?>" width="160" class="flexibleImage" style="max-width: 160px;border: 0;outline: none;text-decoration: none;-ms-interpolation-mode: bicubic;height: auto;">
                                                             </td>
                                                         </tr>
                                                     </table>
@@ -253,7 +253,7 @@ http://templates.mailchimp.com/development/css/outlook-conditional-css
                                                     <table border="0" cellpadding="0" cellspacing="0" width="100%" style="border-collapse: collapse;mso-table-lspace: 0pt;mso-table-rspace: 0pt;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;">
                                                         <tr>
                                                             <td valign="top" class="textContentLast" style="mso-table-lspace: 0pt;mso-table-rspace: 0pt;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;color: #404040;font-family: Helvetica;font-size: 12px;line-height: 125%;text-align: center;padding-bottom: 20px;">
-                                                                <?= __('This email is an automatic notification sent by'); ?> <a href="<?php echo $fullBaseUrl ?>" style="-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;color: #888888;text-decoration: underline;"><?php echo $fullBaseUrl ?></a>.
+                                                                <?= __('This email is an automatic notification sent by'); ?> <a href="<?php echo Purifier::clean($fullBaseUrl) ?>" style="-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;color: #888888;text-decoration: underline;"><?php echo Purifier::clean($fullBaseUrl) ?></a>.
                                                                 <?= __('You can disable these notifications by requesting an administrator to delete your account.'); ?>
                                                             </td>
                                                         </tr>
