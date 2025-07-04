4e9b29b6e6447fcf6ceebe81546adf4bda10e456
Ishan Vyas <ishan@passbolt.com>
2025-07-04 19:59:32 +0530

    PB-43558 Prepare v5.3.0-rc.1 release

--- config/version.php
+++ config/version.php
@@ -1,8 +1,8 @@
 <?php
 return [
     'passbolt' => [
-        'version' => '5.3.0-test.1',
-        'name' => 'TBD',
+        'version' => '5.3.0-rc.1',
+        'name' => 'Riders on the Storm',
     ],
     'php' => [
         'minVersion' => '8.2',
