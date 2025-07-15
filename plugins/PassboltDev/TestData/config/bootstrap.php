a88e3c731ea1f197cb3744d2c57401563ff260c7
Juan Pablo Ramirez <pabloelcolombiano@gmail.com>
2025-07-15 17:18:20 +0200

    PB-43525 Refactor test data plugin config

--- plugins/PassboltDev/TestData/config/bootstrap.php
+++ plugins/PassboltDev/TestData/config/bootstrap.php
@@ -3,3 +3,14 @@
 use Cake\Core\Configure;
 
 Configure::load('Passbolt/TestData.config', 'default', true);
+
+// Test gpg keys folder path.
+if (!defined('PASSBOLT_TEST_DATA_GPGKEY_PATH')) {
+    define('PASSBOLT_TEST_DATA_GPGKEY_PATH', __DIR__ . DS . 'gpg');
+}
+// Test avatars folder path.
+if (!defined('PASSBOLT_TEST_DATA_AVATAR_PATH')) {
+    define('PASSBOLT_TEST_DATA_AVATAR_PATH', __DIR__ . DS . 'img' . DS . 'avatar');
+}
+
+Configure::load('PassboltTestData.version', 'default', true);
