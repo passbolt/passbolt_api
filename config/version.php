f3e0d2c5df318d22b892a43f27d346824b510531
Ishan Vyas <ishan@passbolt.com>
2025-08-07 19:10:04 +0530

    PB-44494 Prepare v5.4.0-test.1 release

--- config/version.php
+++ config/version.php
@@ -1,8 +1,8 @@
 <?php
 return [
     'passbolt' => [
-        'version' => '5.3.2',
-        'name' => 'Somebody to love',
+        'version' => '5.4.0-test.1',
+        'name' => 'Bon Jovi - It\'s my life',
     ],
     'php' => [
         'minVersion' => '8.2',
