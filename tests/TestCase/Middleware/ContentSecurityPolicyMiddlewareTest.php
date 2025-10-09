82a86add9fc8eb9df2a028e4241284d407f12635
jpramirez <pabloelcolombiano@gmail.com>
2025-10-09 11:54:12 +0000

    PB-45158 Adds frame-ancestors:none and form-action:self to the CSP header

---
+++ tests/TestCase/Middleware/ContentSecurityPolicyMiddlewareTest.php
@@ -0,0 +1,63 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * Passbolt ~ Open source password manager for teams
+ * Copyright (c) Passbolt SA (https://www.passbolt.com)
+ *
+ * Licensed under GNU Affero General Public License version 3 of the or any later version.
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Passbolt SA (https://www.passbolt.com)
+ * @license       https://opensource.org/licenses/AGPL-3.0 AGPL License
+ * @link          https://www.passbolt.com Passbolt(tm)
+ * @since         5.7.0
+ */
+
+namespace App\Test\TestCase\Middleware;
+
+use App\Middleware\ContentSecurityPolicyMiddleware;
+use App\Test\Lib\Http\TestRequestHandler;
+use Cake\Core\Configure;
+use Cake\Http\ServerRequest;
+use Cake\TestSuite\TestCase;
+
+/**
+ * @covers \App\Middleware\ContentSecurityPolicyMiddleware
+ */
+class ContentSecurityPolicyMiddlewareTest extends TestCase
+{
+    public function testContentSecurityPolicyMiddleware_Default_CSP()
+    {
+        $middleware = new ContentSecurityPolicyMiddleware();
+
+        /** @var \Cake\Http\ServerRequest $response */
+        $response = $middleware->process(new ServerRequest(), new TestRequestHandler());
+
+        $cspHeaders = $response->getHeader('Content-Security-Policy');
+        $expectedHeaders = [
+            "default-src 'self'",
+            "script-src 'self'",
+            "style-src 'self' 'unsafe-inline'",
+            "img-src 'self'",
+            "frame-src 'self' https://*.duosecurity.com",
+            "frame-ancestors 'none'",
+            "form-action 'self'",
+        ];
+        $this->assertSame($expectedHeaders, $cspHeaders);
+    }
+
+    public function testContentSecurityPolicyMiddleware_Config_Overwrite()
+    {
+        $csp = 'foo';
+        Configure::write('passbolt.security.csp', $csp);
+        $middleware = new ContentSecurityPolicyMiddleware();
+
+        /** @var \Cake\Http\ServerRequest $response */
+        $response = $middleware->process(new ServerRequest(), new TestRequestHandler());
+
+        $cspHeaders = $response->getHeader('Content-Security-Policy');
+        $this->assertSame([$csp], $cspHeaders);
+    }
+}
