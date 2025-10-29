c378a53a06e5c15d3ed1837c9c534977d53fe67c
jpramirez <pabloelcolombiano@gmail.com>
2025-10-29 07:18:14 +0000

    PB-46073 SH - WP3.1 Implement migration to create secret_revisions table

---
+++ config/Migrations/20251028213926_V570CreateSecretRevisions.php
@@ -0,0 +1,80 @@
+<?php
+declare(strict_types=1);
+/**
+ * Passbolt ~ Open source password manager for teams
+ * Copyright (c) Passbolt SA (https://www.passbolt.com)
+ *
+ * Licensed under GNU Affero General Public License version 3 of the or any later version.
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Passbolt SA (https://www.passbolt.com)
+ * @license       https://opensource.org/licenses/AGPL-3.0 AGPL License
+ * @link          https://www.passbolt.com Passbolt(tm)
+ * @since         5.7.0
+ */
+// @codingStandardsIgnoreStart
+use Migrations\AbstractMigration;
+
+class V570CreateSecretRevisions extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * https://book.cakephp.org/migrations/4/en/migrations.html#the-change-method
+     * @return void
+     */
+    public function change(): void
+    {
+        $this
+            ->table('secret_revisions', ['id' => false, 'primary_key' => ['id'], 'collation' => 'utf8mb4_unicode_ci'])
+            ->addColumn('id', 'uuid', [
+                'default' => null,
+                'null' => false,
+                'encoding' => 'ascii',
+                'collation' => 'ascii_general_ci',
+            ])
+            ->addColumn('resource_id', 'uuid', [
+                'default' => null,
+                'null' => false,
+                'encoding' => 'ascii',
+                'collation' => 'ascii_general_ci', // required for FK, needs to be same as reference table
+            ])
+            ->addColumn('resource_type_id', 'uuid', [
+                'default' => null,
+                'null' => false,
+                'encoding' => 'ascii',
+                'collation' => 'ascii_general_ci', // required for FK, needs to be same as reference table
+            ])
+            ->addColumn('deleted', 'datetime', [
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+            ])
+            ->addColumn('created', 'datetime', [
+                'default' => null,
+                'limit' => null,
+                'null' => false,
+            ])
+            ->addColumn('modified', 'datetime', [
+                'default' => null,
+                'limit' => null,
+                'null' => false,
+            ])
+            ->addColumn('created_by', 'uuid', [
+                'default' => null,
+                'null' => true,
+                'encoding' => 'ascii',
+                'collation' => 'ascii_general_ci', // required for FK, needs to be same as reference table (i.e. users)
+            ])
+            ->addColumn('modified_by', 'uuid', [
+                'default' => null,
+                'null' => true,
+                'encoding' => 'ascii',
+                'collation' => 'ascii_general_ci', // required for FK, needs to be same as reference table (i.e. users)
+            ])
+            ->addIndex(['resource_id'])
+            ->create();
+    }
+}
