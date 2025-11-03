1b9ababe7b4af85e471f73dc13de6aa32d686d94
Juan Pablo Ramirez <pabloelcolombiano@gmail.com>
2025-11-03 11:07:23 +0100

    PB-46281 v5.6.1-test.1
     # fix

--- config/version.php
+++ config/version.php
@@ -1,8 +1,8 @@
 <?php
 return [
     'passbolt' => [
-        'version' => '5.6.0',
-        'name' => 'Big Jet Plane',
+        'version' => '5.6.1-test.1',
+        'name' => 'TBD',
     ],
     'php' => [
         'minVersion' => '8.2',
