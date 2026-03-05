6c3569ded358ba6a5b2ff6fdeb1210cd2d04bc28
Juan Pablo Ramirez <pabloelcolombiano@gmail.com>
2026-03-05 11:29:57 +0100

    Revert "PB-40266 Health-check issues on Ubuntu 24 when running while being in a directory without the +x permission bit for www-data user (GITHUB #571)"
    This reverts commit e5fe03c7811c811de4d39d3de55f98cfb0c1db91.

--- bin/cake.php
+++ bin/cake.php
@@ -7,9 +7,6 @@ require dirname(__DIR__) . '/vendor/autoload.php';
 use App\Application;
 use Cake\Console\CommandRunner;
 
-// Ensure CWD is the project root to avoid proc_open permission errors (e.g. when invoked from /root)
-chdir(dirname(__DIR__));
-
 // Build the runner with an application and root executable name.
 $runner = new CommandRunner(new Application(dirname(__DIR__) . '/config'), 'cake');
 exit($runner->run($argv));
